95b0641bed23a53ff118201a89f7e0e5e0b27676
Conal Elliott <conal@conal.net>
2016-08-17 08:03:54 -0700

    OkL2

--- src/LMap.hs
+++ src/LMap.hs
@@ -34,34 +34,36 @@ import Basis
 class    (Mod s u, HasBasis u, HasTrie (Basis u)) => OkL s u
 instance (Mod s u, HasBasis u, HasTrie (Basis u)) => OkL s u
 
+type OkL2 s u v = (OkL s u, OkL s v)
+
 type LMap' u v = Basis u :->: v
 
 -- | Linear map, represented as an optional memo-trie from basis to
 -- values
-data LMap s u v = (OkL s u, OkL s v) => LMap { unLMap :: LMap' u v }
+data LMap s u v = OkL2 s u v => LMap { unLMap :: LMap' u v }
 
-inLMap :: (OkL s c, OkL s d) =>
+inLMap :: OkL2 s c d =>
           (LMap' a b -> LMap' c d) -> (LMap s a b -> LMap s c d)
 inLMap h (LMap ab) = LMap (h ab)
 
-inLMap2 :: (OkL s e, OkL s f) =>
+inLMap2 :: OkL2 s e f =>
            (LMap' a b -> LMap' c d -> LMap' e f)
         -> (LMap s a b -> LMap s c d -> LMap s e f)
 inLMap2 h (LMap ab) (LMap cd) = LMap (h ab cd)
 
 -- | Function (assumed linear) as linear map. Only sampled on basis.
-linear :: (OkL s u, OkL s v) => (u -> v) -> LMap s u v
+linear :: OkL2 s u v => (u -> v) -> LMap s u v
 linear f = LMap (trie (f . basisValue))
 
 -- | Apply a linear map to a vector.
-lapply :: (OkL s u, OkL s v) => LMap s u v -> (u -> v)
+lapply :: OkL2 s u v => LMap s u v -> (u -> v)
 lapply (LMap tr) = linearCombo . map (first (untrie tr)) . decompose
 
-instance (OkL s u, OkL s v) => Additive (LMap s u v) where
+instance OkL2 s u v => Additive (LMap s u v) where
   zero  = linear zero
   (^+^) = inLMap2 (^+^)
 
-instance (OkL s u, OkL s v) => Semimodule (LMap s u v) where
+instance OkL2 s u v => Semimodule (LMap s u v) where
   type Scalar (LMap s u v) = Scalar v
   s *^ m = m . scaleL s
 
@@ -99,7 +101,7 @@ instance CoproductCat (LMap s) where
   type Coprod (LMap s) = (:*)  -- direct sum
   inl = linear (, zero)
   inr = linear (zero ,)
-  f ||| g = linear (joinF (lapply f) (lapply g))
+  f ||| g = linear (lapply f `joinF` lapply g)
 
 joinF :: Additive c => (a -> c) -> (b -> c) -> (a :* b -> c)
 (f `joinF` g) (a,b) = f a ^+^ g b
