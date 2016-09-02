a213558728171c8aa3fc6b378a700e4eec1cf397
Conal Elliott <conal@conal.net>
2016-09-02 14:51:11 -0700

    dropped newtype wrapper. much simpler!

--- src/TrieCat.hs
+++ src/TrieCat.hs
@@ -11,6 +11,7 @@
 
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 -- | Tries as category
 
@@ -28,64 +29,36 @@ import ConCat
     Memo trie functors
 --------------------------------------------------------------------}
 
-infixr 0 -->
-newtype a --> b = Memod (a :->: b)
+instance Category (:->:) where
+  type Ok (:->:) = HasTrie
+  id = idTrie
+  (.) = (@.@)
+
+instance OpCon (:*) HasTrie where inOp = Sub Dict
+instance OpCon (:+) HasTrie where inOp = Sub Dict
+
+instance ProductCat (:->:) where
+  exl   = trie exl
+  exr   = trie exr
+  (&&&) = inTrie2 (&&&)
+
+instance CoproductCat (:->:) where
+  inl   = trie inl
+  inr   = trie inr
+  (|||) = inTrie2 (|||)
 
 #if 1
-class HasTrie a => OkM a
-instance HasTrie a => OkM a
-#else
-type OkM = HasTrie
-#endif
 
--- Using OkM in place of HasTrie avoids orphan instances for
--- OpCon (:*) HasTrie and OpCon (:+) HasTrie.
+instance OpCon (:->:) HasTrie where -- inOp = Sub Dict
 
-instance Newtype (a --> b) where
-  type O (a --> b) = a :->: b
-  unpack (Memod t) = t
-  pack t = Memod t
-
-instance Category (-->) where
-  type Ok (-->) = OkM
-  id  = pack idTrie
-  (.) = (inNew2.inTrie2) (.)
-
-instance OpCon (:*) OkM where inOp = Sub Dict
-instance OpCon (:+) OkM where inOp = Sub Dict
-
-instance ProductCat (-->) where
-  exl   = (pack.trie) exl
-  exr   = (pack.trie) exr
-  (&&&) = (inNew2.inTrie2) (&&&)
-
-instance CoproductCat (-->) where
-  inl   = (pack.trie) inl
-  inr   = (pack.trie) inr
-  (|||) = (inNew2.inTrie2) (|||)
-
-#if 0
-
-instance OpCon (-->) OkM where inOp = Sub Dict
-
-instance ClosedCat (-->) where
-  type Exp (-->) = (-->)
-  apply :: forall a b. Ok2 (-->) a b => Exp (-->) a b :* a --> b
-  apply = (pack.trie) (apply . first (untrie.unpack))
-    <+ inOp @(Exp (-->)) @(Ok (-->)) @a @b
-  curry = pack . fmap pack . unpack . unpack
-  uncurry = pack . pack . fmap unpack . unpack
+instance ClosedCat (:->:) where
+  type Exp (:->:) = (:->:)
+  apply :: forall a b. Ok2 (:->:) a b => Exp (:->:) a b :* a :->: b
+  apply = trie (apply . first untrie)
+    <+ inOp @(Exp (:->:)) @(Ok (:->:)) @a @b
+  curry = unpack
+  uncurry = pack
 
   -- apply = (pack.trie) (\ (Memod t, a) -> untrie t a)
-
-#if 0
-first (untrie . unpack) :: (a --> b) :* a -> (a -> b) :* a
-apply                   :: ''             -> (a --> b) :* a -> b
-
-unpack    :: (a :* b --> c) -> (a :* b :->: c)
-unpack    :: ''             -> (a :->: b :->: x)
-fmap pack :: ''             -> (a :->: b --> x)
-pack      :: ''             -> (a --> b --> x)
-#endif
 
 #endif
