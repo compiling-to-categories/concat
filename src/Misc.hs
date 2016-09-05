efc465ee6903963d1305f54cc6b88bd382e1f6a3
Conal Elliott <conal@conal.net>
2016-09-04 18:56:04 -0700

    helpers for GHC.Generics

--- src/Misc.hs
+++ src/Misc.hs
@@ -8,6 +8,7 @@
 
 module Misc where
 
+import GHC.Generics ((:+:)(..),(:*:)(..))
 import Control.Newtype
 
 {--------------------------------------------------------------------
@@ -25,6 +26,33 @@ type (:+)  = Either
 type (:=>) = (->)
 
 {--------------------------------------------------------------------
+    Helpers for GHC.Generics
+--------------------------------------------------------------------}
+
+prodF :: a t :* b t -> (a :*: b) t
+prodF = uncurry (:*:)
+
+unProdF :: (a :*: b) t -> a t :* b t
+unProdF (a :*: b) = (a,b)
+
+eitherF :: (a t -> c) -> (b t -> c) -> ((a :+: b) t -> c)
+eitherF f _ (L1 a) = f a
+eitherF _ g (R1 b) = g b
+
+sumF :: a t :+ b t -> (a :+: b) t
+sumF = either L1 R1
+
+unSumF :: (a :+: b) t -> a t :+ b t
+unSumF = eitherF Left Right
+
+infixr 1 +->
+infixr 0 $*
+data (a +-> b) p = Fun1 { ($*) :: a p -> b p }
+
+-- TODO: resolve name conflict with tries. Using ":->:" for functors fits with
+-- other type constructors in GHC.Generics.
+
+{--------------------------------------------------------------------
     Other
 --------------------------------------------------------------------}
 
@@ -40,12 +68,12 @@ type (:=>) = (->)
 class    Yes a
 instance Yes a
 
-inNew :: (Newtype n, Newtype n') =>
-         (O n -> O n') -> (n -> n')
+inNew :: (Newtype p, Newtype q) =>
+         (O p -> O q) -> (p -> q)
 inNew = pack <~ unpack
 
-inNew2 :: (Newtype n, Newtype n', Newtype n'') =>
-          (O n -> O n' -> O n'') -> (n -> n' -> n'')
+inNew2 :: (Newtype p, Newtype q, Newtype r) =>
+          (O p -> O q -> O r) -> (p -> q -> r)
 inNew2 = inNew <~ unpack
 
 -- TODO: use inNew and inNew2 in place of ad hoc versions throughout.
