12f065019a3cd1ba41f5b690890c30d903c9f0b9
Conal Elliott <conal@conal.net>
2016-09-08 13:24:17 -0700

    Non-free linear maps wrapped for ConCat

---
+++ src/PostLinear.hs
@@ -0,0 +1,89 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE Rank2Types #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE ExistentialQuantification #-}
+
+-- TODO: trim extensions
+
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
+
+-- | Linear maps for the blog post(s)
+
+module PostLinear where
+
+import Prelude hiding (id,(.),curry,uncurry)
+import qualified Prelude as P
+import Control.Arrow (Kleisli(..),arr)
+import qualified Control.Arrow as A
+import Control.Applicative (liftA2)
+import Control.Monad ((<=<))
+import qualified Control.Category as Cat
+-- import Data.Constraint (Dict(..),(:-)(..))
+import Control.Newtype
+import GHC.Types (Constraint)
+import Data.MemoTrie
+
+import Data.VectorSpace
+import Data.LinearMap
+import Data.Basis
+
+import Data.Constraint hiding ((&&&),(***),(:=>))
+import qualified Data.Constraint as C
+
+import Misc hiding ((<~))
+import ConCat
+
+class    (VectorSpace a, Scalar a ~ s, HasBasis a, HasTrie (Basis a)) => OkL s a
+instance (VectorSpace a, Scalar a ~ s, HasBasis a, HasTrie (Basis a)) => OkL s a
+
+type OkL2 s a b = C2 (OkL s) a b
+
+-- | Linear map over a given scalar field
+data LMap s a b = OkL2 s a b => LMap (a :-* b)
+
+-- Needs ExistentialQuantification
+
+instance OkL2 s a b => Newtype (LMap s a b) where
+  type O (LMap s a b) = a :-* b
+  pack t = LMap t
+  unpack (LMap t) = t
+
+instance Category (LMap s) where
+  type Ok (LMap s) = OkL s
+  id  = pack idL
+  (.) = inNew2 (*.*)
+
+instance OpCon (:*) (OkL s) where inOp = Sub Dict
+
+-- exlL  :: ... => a :* b :-* a
+-- exRL  :: ... => a :* b :-* b
+-- forkL :: ... => (a :-* c) -> (a :-* d) -> (a :-* c :* d)
+
+instance ProductCat (LMap s) where
+  type Prod (LMap s) = (:*)
+  exl   = pack exlL
+  exr   = pack exrL
+  (&&&) = inNew2 forkL
+
+-- inlL  :: ... => a :-* a :* b
+-- inrL  :: ... => b :-* a :* b
+-- joinL :: ... => (a :-* c) -> (b :-* c) -> (a :* b :-* c)
+
+instance CoproductCat (LMap s) where
+  type Coprod (LMap s) = (:*)
+  inl   = pack inlL
+  inr   = pack inrL
+  (|||) = inNew2 joinL
