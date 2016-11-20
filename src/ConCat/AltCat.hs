c2181735af922fa22880a124bb5c9605a990fb67
Conal Elliott <conal@conal.net>
2016-11-19 16:12:41 -0800

    more ops

--- src/ConCat/AltCat.hs
+++ src/ConCat/AltCat.hs
@@ -8,93 +8,124 @@
 -- | Alternative interface to the class operations from ConCat.Category, so as
 -- not to get inlined too eagerly to optimize.
 
-module ConCat.AltCat where
+module ConCat.AltCat
+  ( module ConCat.AltCat
+  , module C)
+  where
 
 {-# LANGUAGE NoMonomorphismRestriction #-}
 
 {-# OPTIONS_GHC -Wall #-}
-
-{-# OPTIONS_GHC -fno-warn-unused-imports #-}  -- TEMP
-
+-- {-# OPTIONS_GHC -fno-warn-unused-imports #-}  -- TEMP
 
 import Prelude hiding (id,(.),curry,uncurry,const)
 
 import ConCat.Category
   ( Category, Ok,Ok2,Ok3,Ok4,Ok5
-  , ProductCat, Prod
-  , CoproductCat, Coprod
+  , ProductCat, Prod, twiceP, inLassocP, inRassocP, transposeP, unfork
+  , CoproductCat, Coprod, inLassocS, inRassocS, transposeS, unjoin
+  , DistribCat, undistl, undistr
   , ClosedCat, Exp
+  , TerminalCat, Unit, lunit, runit, constFun, constFun2, unitFun, unUnitFun
+  , ConstCat, ConstObj, lconst, rconst
+  , BiCCC
+  , BoolCat, BoolOf
+  , NumCat, FractionalCat, FloatingCat, FromIntegralCat
+  , EqCat, OrdCat, EnumCat, BottomCat, IfCat, UnknownCat, RepCat
+  , ccc
   )
+
 import qualified ConCat.Category as C
+import ConCat.Rep
 
 #define Op(nm,ty) \
-id :: ty; \
+nm :: ty; \
 nm = C.nm ;\
 {-# NOINLINE [2] nm #-}
 
--- id :: (Category k, Ok k a) => a `k` a
--- id = C.id
--- {-# NOINLINE [2] id    #-}
+#define InfixOp(nm,ty) \
+(nm) :: ty; \
+(nm) = (C.nm) ;\
+{-# NOINLINE [2] (nm) #-}
 
-Op(id,(Category k, Ok k a) => a `k` a)
 
 infixr 9 .
-(.) :: forall k b c a. (Category k, Ok3 k a b c) => (b `k` c) -> (a `k` b) -> (a `k` c)
-(.) = (C..)
-{-# NOINLINE [2] (.)   #-}
-
-
-exl :: (ProductCat k, Ok2 k a b) => Prod k a b `k` a
-exl = C.exl
-{-# NOINLINE [2] exl #-}
-
-exr :: (ProductCat k, Ok2 k a b) => Prod k a b `k` b
-exr = C.exr
-{-# NOINLINE [2] exr #-}
+Op(id,(Category k, Ok k a) => a `k` a)
+InfixOp(.,forall k b c a. (Category k, Ok3 k a b c) => (b `k` c) -> (a `k` b) -> (a `k` c))
 
 infixr 3 ***, &&&
-
-(&&&) :: forall k a c d. (ProductCat k, Ok3 k a c d)
-      => (a `k` c) -> (a `k` d) -> (a `k` Prod k c d)
-(&&&) = (C.&&&)
-{-# NOINLINE [2] (&&&) #-}
-
-(***) :: forall k a b c d. (ProductCat k, Ok4 k a b c d)
-      => (a `k` c) -> (b `k` d) -> (Prod k a b `k` Prod k c d)
-(***) = (C.***)
-{-# NOINLINE [2] (***) #-}
-
+Op(exl,(ProductCat k, Ok2 k a b) => Prod k a b `k` a)
+Op(exr,(ProductCat k, Ok2 k a b) => Prod k a b `k` b)
+InfixOp(&&&,forall k a c d. (ProductCat k, Ok3 k a c d) => (a `k` c) -> (a `k` d) -> (a `k` Prod k c d))
+InfixOp(***,forall k a b c d. (ProductCat k, Ok4 k a b c d) => (a `k` c) -> (b `k` d) -> (Prod k a b `k` Prod k c d))
+Op(swapP,forall k a b. (ProductCat k, Ok2 k a b) => Prod k a b `k` Prod k b a)
+Op(first,forall k a aa b. (ProductCat k, Ok3 k a b aa) => (a `k` aa) -> (Prod k a b `k` Prod k aa b))
+Op(second,forall k a b bb. (ProductCat k, Ok3 k a b bb) => (b `k` bb) -> (Prod k a b `k` Prod k a bb))
+Op(lassocP,forall k a b c. (ProductCat k, Ok3 k a b c) => Prod k a (Prod k b c) `k` Prod k (Prod k a b) c)
+Op(rassocP,forall k a b c. (ProductCat k, Ok3 k a b c) => Prod k (Prod k a b) c `k` Prod k a (Prod k b c))
 
 infixr 2 +++, |||
+Op(inl,(CoproductCat k, Ok2 k a b) => a `k` Coprod k a b)
+Op(inr,(CoproductCat k, Ok2 k a b) => b `k` Coprod k a b)
+InfixOp(|||,forall k a c d. (CoproductCat k, Ok3 k a c d) => (c `k` a) -> (d `k` a) -> (Coprod k c d `k` a))
+InfixOp(+++,forall k a b c d. (CoproductCat k, Ok4 k a b c d) => (c `k` a) -> (d `k` b) -> (Coprod k c d `k` Coprod k a b))
+Op(jam,(CoproductCat k, Ok k a) => Coprod k a a `k` a)
+Op(swapS,forall k a b. (CoproductCat k, Ok2 k a b) => Coprod k a b `k` Coprod k b a)
+Op(left ,forall k a aa b. (CoproductCat k, Ok3 k a b aa) => (a `k` aa) -> (Coprod k a b `k` Coprod k aa b))
+Op(right,forall k a b bb. (CoproductCat k, Ok3 k a b bb) => (b `k` bb) -> (Coprod k a b `k` Coprod k a bb))
+Op(lassocS,forall k a b c. (CoproductCat k, Ok3 k a b c) => Coprod k a (Coprod k b c) `k` Coprod k (Coprod k a b) c)
+Op(rassocS,forall k a b c. (CoproductCat k, Ok3 k a b c) => Coprod k (Coprod k a b) c `k` Coprod k a (Coprod k b c))
 
-inl :: (CoproductCat k, Ok2 k a b) => a `k` Coprod k a b
-inl = C.inl
-{-# NOINLINE [2] inl #-}
+Op(apply,forall k a b. (ClosedCat k, Ok2 k a b) => Prod k (Exp k a b) a `k` b)
+Op(curry,(ClosedCat k, Ok3 k a b c) => (Prod k a b `k` c) -> (a `k` Exp k b c))
+Op(uncurry,forall k a b c. (ClosedCat k, Ok3 k a b c) => (a `k` Exp k b c)  -> (Prod k a b `k` c))
 
-inr :: (CoproductCat k, Ok2 k a b) => b `k` Coprod k a b
-inr = C.inr
-{-# NOINLINE [2] inr #-}
+Op(distl,forall k a u v. (DistribCat k, Ok3 k a u v) => Prod k a (Coprod k u v) `k` Coprod k (Prod k a u) (Prod k a v))
+Op(distr,forall k u v b. (DistribCat k, Ok3 k u v b) => Prod k (Coprod k u v) b `k` Coprod k (Prod k u b) (Prod k v b))
 
-(|||) :: forall k a c d. (CoproductCat k, Ok3 k a c d)
-      => (c `k` a) -> (d `k` a) -> (Coprod k c d `k` a)
-(|||) = (C.|||)
-{-# NOINLINE [2] (|||) #-}
+Op(it,(TerminalCat k, Ok k a) => a `k` Unit k)
 
-(+++) :: forall k a b c d. (CoproductCat k, Ok4 k a b c d)
-      => (c `k` a) -> (d `k` b) -> (Coprod k c d `k` Coprod k a b)
-(+++) = (C.+++)
-{-# NOINLINE [2] (+++) #-}
+Op(unitArrow,ConstCat k b => b -> (Unit k `k` ConstObj k b))
+Op(const,(ConstCat k b, Ok k a) => b -> (a `k` ConstObj k b))
+
+Op(notC,BoolCat k => BoolOf k `k` BoolOf k)
+Op(andC,BoolCat k => Prod k (BoolOf k) (BoolOf k) `k` BoolOf k)
+Op(orC,BoolCat k => Prod k (BoolOf k) (BoolOf k) `k` BoolOf k)
+Op(xorC,BoolCat k => Prod k (BoolOf k) (BoolOf k) `k` BoolOf k)
+
+Op(negateC,NumCat k a => a `k` a)
+Op(addC,NumCat k a => Prod k a a `k` a)
+Op(subC,NumCat k a => Prod k a a `k` a)
+Op(mulC,NumCat k a => Prod k a a `k` a)
+Op(powIC,NumCat k a => Prod k a Int `k` a)
+
+Op(recipC,FractionalCat k a => a `k` a)
+Op(divideC,FractionalCat k a => Prod k a a `k` a)
+
+Op(expC,FloatingCat k a => a `k` a)
+Op(cosC,FloatingCat k a => a `k` a)
+Op(sinC,FloatingCat k a => a `k` a)
+
+Op(fromIntegralC,FromIntegralCat k a b => a `k` b)
+
+Op(equal,EqCat k a => Prod k a a `k` BoolOf k)
+Op(notEqual,EqCat k a => Prod k a a `k` BoolOf k)
+
+Op(lessThan,OrdCat k a => Prod k a a `k` BoolOf k)
+Op(greaterThan,OrdCat k a => Prod k a a `k` BoolOf k)
+Op(lessThanOrEqual,OrdCat k a => Prod k a a `k` BoolOf k)
+Op(greaterThanOrEqual,OrdCat k a => Prod k a a `k` BoolOf k)
+
+Op(succC,EnumCat k a => a `k` a)
+Op(predC,EnumCat k a => a `k` a)
+
+Op(bottomC,BottomCat k a => Unit k `k` a)
+
+Op(ifC,IfCat k a => Prod k (BoolOf k) (Prod k a a) `k` a)
+
+Op(unknownC,UnknownCat k a b => a `k` b)
+
+Op(reprC,(RepCat k, HasRep a) => a `k` Rep a)
+Op(abstC,(RepCat k, HasRep a) => Rep a `k` a)
 
 
-apply :: forall k a b. (ClosedCat k, Ok2 k a b) => Prod k (Exp k a b) a `k` b
-apply = C.apply
-{-# NOINLINE [2] apply #-}
-
-curry :: (ClosedCat k, Ok3 k a b c) => (Prod k a b `k` c) -> (a `k` Exp k b c)
-curry = C.curry
-{-# NOINLINE [2] curry #-}
-
-uncurry :: forall k a b c. (ClosedCat k, Ok3 k a b c)
-        => (a `k` Exp k b c)  -> (Prod k a b `k` c)
-uncurry = C.uncurry
-{-# NOINLINE [2] uncurry #-}
