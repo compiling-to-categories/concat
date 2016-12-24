47a83414b9b9dfb60a4020af68351f7e56d5d288
Conal Elliott <conal@conal.net>
2016-12-24 14:00:56 -0800

    trim and elaborate

--- src/ConCat/Rebox.hs
+++ src/ConCat/Rebox.hs
@@ -1,69 +1,55 @@
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeInType #-}
 {-# LANGUAGE MagicHash #-}
 
 {-# OPTIONS_GHC -Wall #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
 
 {-# OPTIONS_GHC -Wno-orphans -Wno-inline-rule-shadowing #-}
 
--- | Some experiments with reboxing
-
-{-# OPTIONS_GHC -ddump-simpl #-}
-{-# OPTIONS_GHC -dsuppress-idinfo #-}
-{-# OPTIONS_GHC -dsuppress-uniques #-}
-{-# OPTIONS_GHC -dsuppress-module-prefixes #-}
+-- | Reboxing experiments
 
 module ConCat.Rebox where
 
 import GHC.Types
 import GHC.Prim
 
-fooI :: Int -> Int
-fooI x = 3 * x + x
+import ConCat.AltCat
 
-barI :: Int -> Int -> Int
-barI x y = 3 * x + y + 7
+boxI :: Int# -> Int
+boxI = I#
+{-# INLINE [0] boxI #-}
 
-fooI' :: Int -> Int
-fooI' = fooI . reI
+boxF :: Float# -> Float
+boxF = F#
+{-# INLINE [0] boxF #-}
 
-mkI :: Int# -> Int
-mkI = I#
-{-# NOINLINE mkI #-}
-
-unI :: Int -> Int#
-unI (I# x) = x
--- {-# NOINLINE unI #-}
-
-fooI'' :: Int -> Int
-fooI'' x  = fooI (mkI (unI x))
-
--- fooF :: Float -> Float
--- fooF x = 3 * x + x
-
-reI :: Int -> Int
-reI y = I# (unI y)
--- reI (I# x) = I# x
-{-# INLINE reI #-}
-
-#if 1
-
-plusI :: Int -> Int -> Int
-plusI = (+)
-{-# NOINLINE plusI #-}
-
-timesI :: Int -> Int -> Int
-timesI = (+)
-{-# NOINLINE timesI #-}
+boxD :: Double# -> Double
+boxD = D#
+{-# INLINE [0] boxD #-}
 
 {-# RULES
 
-"I# +" forall u v. I# (u +# v) = I# u `plusI` I# v
-"I# *" forall u v. I# (u *# v) = I# u `timesI` I# v
+"boxI negate" forall u. boxI (negateInt# u) = negateC (boxI u)
+"boxI +" forall u v. boxI (u +# v) = addC (boxI u,boxI v)
+"boxI -" forall u v. boxI (u -# v) = subC (boxI u,boxI v)
+"boxI *" forall u v. boxI (u *# v) = mulC (boxI u,boxI v)
 
--- "F# +" forall u v. F# (u `plusFloat#` v) = F# u `addF` F# v
--- "F# *" forall u v. F# (u `timesFloat#` v) = F# u `timesF` F# v
+"boxF negate" forall u. boxF (negateFloat# u) = negateC (boxF u)
+"boxF +" forall u v. boxF (u `plusFloat#`  v) = addC (boxF u,boxF v)
+"boxF -" forall u v. boxF (u `minusFloat#` v) = subC (boxF u,boxF v)
+"boxF *" forall u v. boxF (u `timesFloat#` v) = mulC (boxF u,boxF v)
+"boxF /" forall u v. boxF (u `divideFloat#` v) = divideC (boxF u,boxF v)
+"boxF exp" forall u. boxF (expFloat# u) = expC (boxF u)
+"boxF cos" forall u. boxF (cosFloat# u) = cosC (boxF u)
+"boxF sin" forall u. boxF (sinFloat# u) = sinC (boxF u)
+
+"boxD negate" forall u. boxD (negateDouble# u) = negateC (boxD u)
+"boxD +" forall u v. boxD (u +## v) = addC (boxD u,boxD v)
+"boxD -" forall u v. boxD (u -## v) = subC (boxD u,boxD v)
+"boxD *" forall u v. boxD (u *## v) = mulC (boxD u,boxD v)
+"boxD /" forall u v. boxD (u /## v) = divideC (boxD u,boxD v)
+"boxD exp" forall u. boxD (expDouble# u) = expC (boxD u)
+"boxD cos" forall u. boxD (cosDouble# u) = cosC (boxD u)
+"boxD sin" forall u. boxD (sinDouble# u) = sinC (boxD u)
 
  #-}
-
-#endif
