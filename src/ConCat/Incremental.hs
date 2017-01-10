2a4a70428f986f31df86b62e0a6c7fe24a623048
Conal Elliott <conal@conal.net>
2017-01-09 17:24:16 -0800

    representation experiment

--- src/ConCat/Incremental.hs
+++ src/ConCat/Incremental.hs
@@ -27,7 +27,8 @@ data Delta :: * -> * where
   IdD    :: Delta a
   (:***) :: Delta a -> Delta b -> Delta (a :* b)
   (:+++) :: Delta a -> Delta b -> Delta (a :+ b)
-
+  -- LeftD  :: Delta a -> Delta (a :+ b)
+  -- RightD :: Delta b -> Delta (a :+ b)
   -- IsoD :: (a -> r) -> (r -> a) -> Delta r -> Delta a
 
 applyD :: Delta a -> Unop a
@@ -35,6 +36,9 @@ applyD (ConstD a)   = const a
 applyD IdD          = id
 applyD (da :*** db) = applyD da *** applyD db
 applyD (da :+++ db) = applyD da +++ applyD db
+
+-- applyD (LeftD  da)  = applyD da +++ error "applyD: LeftD given Right"
+-- applyD (RightD db)  = error "applyD: RightD given Left" +++ applyD db
 -- applyD (IsoD ar ra dr) = ra . applyD dr . ar
 
 instance Monoid (Delta a) where
@@ -52,10 +56,16 @@ splitP IdD            = (IdD,IdD)
 
 splitS :: Delta (a :+ b) -> Delta a :* Delta b
 splitS (da :+++ db)        = (da,db)
-splitS (ConstD (Right db)) = (IdD, ConstD db)
 splitS (ConstD (Left da))  = (ConstD da, IdD)
 splitS (ConstD (Right db)) = (IdD, ConstD db)
 splitS IdD                 = (IdD,IdD)
+
+-- splitS :: Delta (a :+ b) -> Delta a :+ Delta b
+-- splitS (LeftD da)          = Left da
+-- splitS (RightD db)         = Right db
+-- splitS (ConstD (Left  da)) = Left  (ConstD da)
+-- splitS (ConstD (Right db)) = Right (ConstD db)
+-- -- splitS IdD                 = (IdD,IdD)
 
 newtype a +-> b = DX (Delta a -> Delta b)
 
@@ -78,42 +88,56 @@ exlD = exl . splitP
 exrD :: Delta (a :* b) -> Delta b
 exrD = exr . splitP
 
--- Spec: applyD (da `pairD` db) (a,b) = (applyD da a, applyD db b)
-pairD :: Delta a -> Delta b -> Delta (a :* b)
-IdD      `pairD` IdD      = IdD
-ConstD a `pairD` ConstD b = ConstD (a,b)
-da       `pairD` db       = da :*** db
+-- Spec: applyD (joinP (da,db)) (a,b) = (applyD da a, applyD db b)
+joinP :: Delta a :* Delta b -> Delta (a :* b)
+joinP (IdD, IdD)           = IdD
+joinP (ConstD a, ConstD b) = ConstD (a,b)
+joinP (da, db)             = da :*** db
+
+joinS :: Delta a :* Delta b -> Delta (a :+ b)
+joinS (IdD, IdD)           = IdD
+joinS (da, db)             = da :+++ db
 
 -- forkD :: (Delta a -> Delta c) -> (Delta a -> Delta d) -> (Delta a -> Delta (c :* d))
 -- (f `forkD` g) da = f da :*** g da
 
--- pairD :: (Delta a -> Delta c) -> (Delta b -> Delta d) -> (Delta (a :* b) -> Delta (c :* d))
--- (f `pairD` g) (splitP -> (da,db)) =
+-- joinP :: (Delta a -> Delta c) -> (Delta b -> Delta d) -> (Delta (a :* b) -> Delta (c :* d))
+-- (f `joinP` g) (splitP -> (da,db)) =
 --   f da :*** g db
 
 inlD :: Delta a -> Delta (a :+ b)
 inlD da = da :+++ IdD
+-- inlD = LeftD
 
 inrD :: Delta b -> Delta (a :+ b)
 inrD da = IdD :+++ da
+-- inrD = RightD
 
 instance ProductCat (+->) where
   exl = DX (exl . splitP)
   exr = DX (exr . splitP)
-  (&&&) = inNew2 (liftA2 pairD)
-  -- DX f &&& DX g = DX (liftA2 pairD f g)
-  -- DX f &&& DX g = DX (\ da -> f da `pairD` g da)
+  DX f &&& DX g = DX (joinP . (f &&& g))
+  -- (&&&) = inNew2 (\ f g -> uncurry joinP . (f &&& g))
+  -- (&&&) = inNew2 (liftA2 joinP)
+  -- DX f &&& DX g = DX (liftA2 joinP f g)
+  -- DX f &&& DX g = DX (\ da -> f da `joinP` g da)
 
 instance CoproductCat (+->) where
   inl = DX inlD
   inr = DX inrD
   DX f ||| DX g = DX (\ (splitS -> (da,db)) -> f da `mappend` g db)
-  -- (|||) = inNew2 (liftA2 (:+++))
+
+--   DX f ||| DX g = DX ((f ||| g) . splitS)
 
 -- Use AD after generalizing.
 der :: (a -> b) -> (a +-> b)
 der _ = error "der called"
 {-# NOINLINE der #-}
+
+instance Newtype (Delta ()) where
+  type O (Delta ()) = ()
+  unpack = const ()
+  pack = const IdD
 
 #define Atomic(ty) \
 instance Newtype (Delta (ty)) where \
@@ -121,11 +145,6 @@ instance Newtype (Delta (ty)) where \
   ; unpack (ConstD x) = Just x \
   ; unpack IdD = Nothing \
   ; pack = maybe IdD ConstD }
-
-instance Newtype (Delta ()) where
-  type O (Delta ()) = ()
-  unpack = const ()
-  pack = const IdD
 
 Atomic(Bool)
 Atomic(Int)
@@ -135,7 +154,17 @@ Atomic(Double)
 instance Newtype (Delta (a :* b)) where
   type O (Delta (a :* b)) = Delta a :* Delta b
   unpack = splitP
-  pack = uncurry pairD
+  pack   = joinP
+
+instance Newtype (Delta (a :+ b)) where
+  type O (Delta (a :+ b)) = Delta a :* Delta b
+  unpack = splitS
+  pack   = joinS
+
+-- instance Newtype (Delta (a :+ b)) where
+--   type O (Delta (a :+ b)) = Delta a :+ Delta b
+--   unpack = splitS
+--   pack = LeftD ||| RightD
 
 {--------------------------------------------------------------------
     Simpler: use Unop in place of Delta
