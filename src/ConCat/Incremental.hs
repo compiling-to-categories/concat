a82b9f561b734f8fbcecf67ed45fe7e4fd1c9d13
Conal Elliott <conal@conal.net>
2017-01-09 19:16:28 -0800

    HasDelta

--- src/ConCat/Incremental.hs
+++ src/ConCat/Incremental.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE IncoherentInstances #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE ViewPatterns #-}
@@ -5,6 +7,8 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
@@ -14,16 +18,18 @@
 module ConCat.Incremental where
 
 import Prelude hiding (id,(.))
+import qualified Prelude as P
 import Control.Applicative (liftA2)
 
+import Data.Constraint ((:-)(..),Dict(..))
 import Control.Newtype
 
-import ConCat.Misc ((:*),(:+),Unop,inNew,inNew2)
+import ConCat.Misc ((:*),(:+),Unop,inNew,inNew2,Yes1)
 import qualified ConCat.Category
 import ConCat.AltCat hiding (const,curry,uncurry)
 
 data Delta :: * -> * where
-  ConstD :: a        -> Delta a
+  ConstD :: a -> Delta a
   IdD    :: Delta a
   (:***) :: Delta a -> Delta b -> Delta (a :* b)
   (:+++) :: Delta a -> Delta b -> Delta (a :+ b)
@@ -95,8 +101,8 @@ joinP (ConstD a, ConstD b) = ConstD (a,b)
 joinP (da, db)             = da :*** db
 
 joinS :: Delta a :* Delta b -> Delta (a :+ b)
-joinS (IdD, IdD)           = IdD
-joinS (da, db)             = da :+++ db
+joinS (IdD, IdD) = IdD
+joinS (da, db)   = da :+++ db
 
 -- forkD :: (Delta a -> Delta c) -> (Delta a -> Delta d) -> (Delta a -> Delta (c :* d))
 -- (f `forkD` g) da = f da :*** g da
@@ -129,15 +135,13 @@ instance CoproductCat (+->) where
 
 --   DX f ||| DX g = DX ((f ||| g) . splitS)
 
+-- instance ConstCat (+->) b where
+--   const b = DX (const (ConstD b))
+
 -- Use AD after generalizing.
 der :: (a -> b) -> (a +-> b)
 der _ = error "der called"
 {-# NOINLINE der #-}
-
-instance Newtype (Delta ()) where
-  type O (Delta ()) = ()
-  unpack = const ()
-  pack = const IdD
 
 #define Atomic(ty) \
 instance Newtype (Delta (ty)) where \
@@ -167,17 +171,107 @@ instance Newtype (Delta (a :+ b)) where
 --   pack = LeftD ||| RightD
 
 {--------------------------------------------------------------------
-    Simpler: use Unop in place of Delta
+    Another experiment: represent Delta via associated type
 --------------------------------------------------------------------}
 
-newtype a *-> b = DU (Unop a -> Unop b)
+class HasDelta a where
+  type Del a
+  appD :: Del a -> Unop a
+  type Del a = Maybe a
+  default appD :: Del a ~ Maybe a => Del a -> Unop a
+  appD = maybe id const
 
-instance Category (*->) where
-  id = DU id
-  DU g . DU f = DU (g . f)
+-- I could move the Maybe to appD, so that Nothing always means id.
+-- If so, make the default Del a = a
 
--- I can define (&&&), but not exl or exr.
+instance HasDelta () where
+  type Del () = ()
+  appD () = id
 
--- instance ProductCat (*->) where
---   DU f &&& DU g = DU (\ da -> f da *** g da)
+instance HasDelta Bool
+instance HasDelta Int
+instance HasDelta Float
+instance HasDelta Double
+
+instance (HasDelta a, HasDelta b) => HasDelta (a :* b) where
+  type Del (a :* b) = Del a :* Del b
+  appD (da,db) = appD da *** appD db
+
+-- instance (HasDelta a, HasDelta b) => HasDelta (a :+ b) where
+--   type Del (a :+ b) = Del a :* Del b
+--   appD (da,db) = appD da +++ appD db
+
+instance (HasDelta a, HasDelta b) => HasDelta (a :+ b) where
+  type Del (a :+ b) = Del a :+ Del b
+  appD (Left  da) (Left  a) = Left  (appD da a)
+  appD (Right db) (Right b) = Right (appD db b)
+  appD _ _ = error "appD on sum type: Left/Right mismatch"
+
+newtype a -+> b = XD (Del a -> Del b)
+
+instance Newtype (a -+> b) where
+  type O (a -+> b) = Del a -> Del b
+  pack f = XD f
+  unpack (XD f) = f
+
+instance Category (-+>) where
+  -- type Ok (-+>) = Y
+  id = pack id
+  (.) = inNew2 (.)
+
+#if 0
+class    Y a
+instance Y a
+
+instance OpCon op (Sat Y) where
+  inOp = Entail (Sub Dict)
+  {-# INLINE inOp #-}
+#endif
+
+{-
+
+If I use the default Ok (-+>) = Yes1, I get some errors I don't understand:
+
+    • Overlapping instances for Yes1 (Del a) arising from a use of ‘id’
+      Matching instances:
+        instance forall k (a :: k). Yes1 a
+          -- Defined at /Users/conal/Haskell/concat/src/ConCat/Misc.hs:98:10
+      There exists a (perhaps superclass) match:
+        from the context: Ok (-+>) a
+          bound by the type signature for:
+                     id :: Ok (-+>) a => a -+> a
+          at /Users/conal/Haskell/concat/src/ConCat/Incremental.hs:210:3-4
+      (The choice depends on the instantiation of ‘a’
+       To pick the first instance above, use IncoherentInstances
+       when compiling the other instance declarations)
+    • In the first argument of ‘XD’, namely ‘id’
+      In the expression: XD id
+      In an equation for ‘id’: id = XD id
+
+    • Could not deduce (Yes1 (Del a), Yes1 (Del b), Yes1 (Del c))
+        arising from a use of ‘.’
+      from the context: Ok3 (-+>) a b c
+        bound by the type signature for:
+                   (.) :: Ok3 (-+>) a b c => b -+> c -> a -+> b -> a -+> c
+        at /Users/conal/Haskell/concat/src/ConCat/Incremental.hs:211:8
+    • In the first argument of ‘XD’, namely ‘(g . f)’
+      In the expression: XD (g . f)
+      In an equation for ‘.’: (XD g) . (XD f) = XD (g . f)
+
+I'm using IncoherentInstances, but I don't know what will happen.
+If it blows up, switch to Y above, and inquire.
+
+-}
+
+instance ProductCat (-+>) where
+  exl = pack exl
+  exr = pack exr
+  (&&&) = inNew2 (&&&)
+
+instance CoproductCat (-+>) where
+  inl = pack inl
+  inr = pack inr
+  (|||) = inNew2 (|||)
+
+
 
