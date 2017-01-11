20ef4ba0aeb04513e899fcb6c3de8368b0633500
Conal Elliott <conal@conal.net>
2017-01-10 22:29:45 -0800

    boxIB

--- src/ConCat/Rebox.hs
+++ src/ConCat/Rebox.hs
@@ -28,28 +28,41 @@ boxD :: Double# -> Double
 boxD = D#
 {-# INLINE [0] boxD #-}
 
+boxIB :: Int# -> Bool
+boxIB i = tagToEnum# i
+{-# INLINE [0] boxIB #-}
+
 {-# RULES
 
-"boxI negate" forall u. boxI (negateInt# u) = negateC (boxI u)
-"boxI +" forall u v. boxI (u +# v) = addC (boxI u,boxI v)
-"boxI -" forall u v. boxI (u -# v) = subC (boxI u,boxI v)
-"boxI *" forall u v. boxI (u *# v) = mulC (boxI u,boxI v)
+"boxI ==" forall u v   . boxIB (u ==# v)          = equal              (boxI u,boxI v)
+"boxI /=" forall u v   . boxIB (u /=# v)          = notEqual           (boxI u,boxI v)
+"boxI >"  forall u v   . boxIB (u >#  v)          = greaterThan        (boxI u,boxI v)
+"boxI >=" forall u v   . boxIB (u >=# v)          = greaterThanOrEqual (boxI u,boxI v)
+"boxI <"  forall u v   . boxIB (u <#  v)          = lessThan           (boxI u,boxI v)
+"boxI <=" forall u v   . boxIB (u <=# v)          = lessThanOrEqual    (boxI u,boxI v)
 
-"boxF negate" forall u. boxF (negateFloat# u) = negateC (boxF u)
-"boxF +" forall u v. boxF (u `plusFloat#`  v) = addC (boxF u,boxF v)
-"boxF -" forall u v. boxF (u `minusFloat#` v) = subC (boxF u,boxF v)
-"boxF *" forall u v. boxF (u `timesFloat#` v) = mulC (boxF u,boxF v)
-"boxF exp" forall u. boxF (expFloat# u) = expC (boxF u)
-"boxF cos" forall u. boxF (cosFloat# u) = cosC (boxF u)
-"boxF sin" forall u. boxF (sinFloat# u) = sinC (boxF u)
+-- TODO: shorten the OrdCat names
 
-"boxD negate" forall u. boxD (negateDouble# u) = negateC (boxD u)
-"boxD +" forall u v. boxD (u +## v) = addC (boxD u,boxD v)
-"boxD -" forall u v. boxD (u -## v) = subC (boxD u,boxD v)
-"boxD *" forall u v. boxD (u *## v) = mulC (boxD u,boxD v)
-"boxD exp" forall u. boxD (expDouble# u) = expC (boxD u)
-"boxD cos" forall u. boxD (cosDouble# u) = cosC (boxD u)
-"boxD sin" forall u. boxD (sinDouble# u) = sinC (boxD u)
+"boxI negate" forall u . boxI (negateInt# u)      = negateC (boxI u)
+"boxI +" forall u v    . boxI (u +# v)            = addC (boxI u,boxI v)
+"boxI -" forall u v    . boxI (u -# v)            = subC (boxI u,boxI v)
+"boxI *" forall u v    . boxI (u *# v)            = mulC (boxI u,boxI v)
+
+"boxF negate" forall u . boxF (negateFloat# u)    = negateC (boxF u)
+"boxF +" forall u v    . boxF (u `plusFloat#`  v) = addC (boxF u,boxF v)
+"boxF -" forall u v    . boxF (u `minusFloat#` v) = subC (boxF u,boxF v)
+"boxF *" forall u v    . boxF (u `timesFloat#` v) = mulC (boxF u,boxF v)
+"boxF exp" forall u    . boxF (expFloat# u)       = expC (boxF u)
+"boxF cos" forall u    . boxF (cosFloat# u)       = cosC (boxF u)
+"boxF sin" forall u    . boxF (sinFloat# u)       = sinC (boxF u)
+
+"boxD negate" forall u . boxD (negateDouble# u)   = negateC (boxD u)
+"boxD +" forall u v    . boxD (u +## v)           = addC (boxD u,boxD v)
+"boxD -" forall u v    . boxD (u -## v)           = subC (boxD u,boxD v)
+"boxD *" forall u v    . boxD (u *## v)           = mulC (boxD u,boxD v)
+"boxD exp" forall u    . boxD (expDouble# u)      = expC (boxD u)
+"boxD cos" forall u    . boxD (cosDouble# u)      = cosC (boxD u)
+"boxD sin" forall u    . boxD (sinDouble# u)      = sinC (boxD u)
 
 -- These two don't work:
 
