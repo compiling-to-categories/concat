afdae7c9e3f4b5029d5ef8213d57ead5d673db15
Conal Elliott <conal@conal.net>
2017-02-06 12:32:26 -0800

    linear maps and differentiable functions

--- src/ConCat/C.hs
+++ src/ConCat/C.hs
@@ -15,6 +15,7 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE UndecidableSuperClasses #-}
+{-# LANGUAGE LambdaCase #-}
 
 -- {-# LANGUAGE TypeInType #-}
 
@@ -25,14 +26,19 @@
 
 module ConCat.C where
 
-import Prelude hiding (id,(.))
+import Prelude hiding (id,(.),zipWith)
 import qualified Prelude as P
 import Data.Kind
+import GHC.Generics (U1(..),(:*:)(..),(:+:)(..))
 
 import Data.Constraint (Dict(..),(:-)(..),refl,trans,(\\))
+import Control.Newtype
+import Data.Pointed
+import Data.Key
 
-import ConCat.Misc (Yes1)
+import ConCat.Misc (Yes1,inNew,inNew2)
 import ConCat.Free.VectorSpace
+import ConCat.Orphans
 
 type C1 (con :: u -> Constraint) a = con a
 type C2 (con :: u -> Constraint) a b = (con a, con b)
@@ -47,6 +53,10 @@ type Ok4 k a b c d     = C4 (Ok k) a b c d
 type Ok5 k a b c d e   = C5 (Ok k) a b c d e
 type Ok6 k a b c d e f = C6 (Ok k) a b c d e f
 
+{--------------------------------------------------------------------
+    Categories
+--------------------------------------------------------------------}
+
 class Category k where
   type Ok k :: u -> Constraint
   type Ok k = Yes1
@@ -58,52 +68,18 @@ instance Category (->) where
   id  = P.id
   (.) = (P..)
 
-infixr 9 %
-infixr 9 %%
-
-class FunctorC f (src :: u -> u -> Type) (trg :: v -> v -> Type) | f -> src trg where
-  type f %% (a :: u) :: v
-  type OkF f (a :: u) (b :: u) :: Constraint
-  (%) :: forall a b. OkF f a b => f -> src a b -> trg (f %% a) (f %% b)
-
-#if 1
-data HFunctor (t :: * -> *) = HFunctor
-
-instance Functor t => FunctorC (HFunctor t) (->) (->) where
-  type HFunctor t %% a = t a
-  type OkF (HFunctor t) a b = ()
-  (%) HFunctor = fmap
-#else
--- Alternatively, put the `FunctorC` constraint into `HFunctor`:
-data HFunctor (t :: * -> *) = Functor t => HFunctor
-
-instance FunctorC (HFunctor t) (->) (->) where
-  type HFunctor t %% a = t a
-  type OkF (HFunctor t) a b = ()
-  (%) HFunctor = fmap
-#endif
-
-newtype UT s f g = UT (f s -> g s)
-
-toUT :: (HasV s a, HasV s b) => (a -> b) -> UT s (V s a) (V s b)
-toUT f = UT (toV . f . unV)
-
--- unUT :: (HasV s a, HasV s b) => UT s (V s a) (V s b) -> (a -> b)
--- unUT (UT g) = unV . g . toV
-
-data ToUT (s :: Type) = ToUT
-
-instance FunctorC (ToUT s) (->) (UT s) where
-  type ToUT s %% a = V s a
-  type OkF (ToUT s) a b = (HasV s a, HasV s b)
-  (%) ToUT = toUT
-
 -- | Category with product.
 class ({-OpCon (Prod k) (Ok k), -}Category k) => Cartesian k where
   type Prod k :: u -> u -> u
   exl :: Ok2 k a b => Prod k a b `k` a
   exr :: Ok2 k a b => Prod k a b `k` b
   (&&&) :: forall a c d. Ok3 k a c d => (a `k` c) -> (a `k` d) -> (a `k` Prod k c d)
+
+instance Cartesian (->) where
+  type Prod (->) = (,)
+  exl = fst
+  exr = snd
+  (f &&& g) x = (f x, g x)
 
 class FunctorC f src trg => CartesianFunctor f src trg where
   prodToProd :: Dict ((f %% Prod src a b) ~ Prod trg (f %% a) (f %% b))
@@ -126,6 +102,79 @@ class (Category k, Ok k (Unit k)) => Terminal k where
 
 --     • Illegal constraint ‘Ok k (Unit k)’ in a superclass context
 --         (Use UndecidableInstances to permit this)
+
+{--------------------------------------------------------------------
+    Functors
+--------------------------------------------------------------------}
+
+infixr 9 %
+infixr 9 %%
+
+class (Category src, Category trg) =>
+      FunctorC f (src :: u -> u -> Type) (trg :: v -> v -> Type) | f -> src trg where
+  type f %% (a :: u) :: v
+  type OkF f (a :: u) (b :: u) :: Constraint
+  (%) :: forall a b. OkF f a b => f -> src a b -> trg (f %% a) (f %% b)
+
+#if 1
+data HFunctor (t :: * -> *) = HFunctor
+
+instance Functor t => FunctorC (HFunctor t) (->) (->) where
+  type HFunctor t %% a = t a
+  type OkF (HFunctor t) a b = ()
+  (%) HFunctor = fmap
+#else
+-- Alternatively, put the `FunctorC` constraint into `HFunctor`:
+data HFunctor (t :: * -> *) = Functor t => HFunctor
+
+instance FunctorC (HFunctor t) (->) (->) where
+  type HFunctor t %% a = t a
+  type OkF (HFunctor t) a b = ()
+  (%) HFunctor = fmap
+#endif
+
+newtype UT (s :: Type) f g = UT (f s -> g s)
+
+instance Newtype (UT s f g) where
+  type O (UT s f g) = f s -> g s
+  pack h = UT h
+  unpack (UT h) = h
+
+instance Category (UT s) where
+  id = pack id
+  (.) = inNew2 (.)
+
+instance Cartesian (UT s) where
+  type Prod (UT s) = (:*:)
+  exl = pack (\ (fs :*: _ ) -> fs)
+  exr = pack (\ (_  :*: gs) -> gs)
+  (&&&) = inNew2 forkF
+
+forkF :: (a t -> c t) -> (a t -> d t) -> a t -> (c :*: d) t
+forkF = ((fmap.fmap.fmap) pack (&&&))
+
+-- forkF ac ad = \ a -> (ac a :*: ad a)
+-- forkF ac ad = \ a -> pack (ac a,ad a)
+-- forkF ac ad = pack . (ac &&& ad)
+
+instance Cocartesian (UT s) where
+  type Coprod (UT s) = (:+:)
+  inl = pack L1
+  inr = pack R1
+  (|||) = inNew2 eitherF
+
+toUT :: (HasV s a, HasV s b) => (a -> b) -> UT s (V s a) (V s b)
+toUT f = UT (toV . f . unV)
+
+-- unUT :: (HasV s a, HasV s b) => UT s (V s a) (V s b) -> (a -> b)
+-- unUT (UT g) = unV . g . toV
+
+data ToUT (s :: Type) = ToUT
+
+instance FunctorC (ToUT s) (->) (UT s) where
+  type ToUT s %% a = V s a
+  type OkF (ToUT s) a b = (HasV s a, HasV s b)
+  (%) ToUT = toUT
 
 infixr 1 |-
 type (|-) = (:-)
@@ -168,3 +217,78 @@ instance FunctorC MapDict (|-) (->) where
   type MapDict %% a = Dict a
   type OkF MapDict a b = ()
   (%) MapDict = mapDict
+
+
+-- Linear map in row-major form
+data LM s f g = LM (g (f s))
+
+class    (Num s, Pointed a, Zip a) => OkLM s a
+instance (Num s, Pointed a, Zip a) => OkLM s a
+
+instance Category (LM s) where
+  type Ok (LM s) = OkLM s
+
+instance Cartesian (LM s) where
+  type Prod (LM s) = (:*:)
+  
+instance Cocartesian (LM s) where
+  type Coprod (LM s) = (:*:)
+
+-- Differentiable function on vector space with field s
+data D (s :: Type) a b = D (a s -> (b s, a s -> b s))
+
+-- TODO: try a more functorish representation: (a :->: b :*: (a :->: b))
+
+linearD :: (a s -> b s) -> D s a b
+linearD h = D (h &&& const h)
+
+instance Category (D s) where
+  type Ok (D s) = OkLM s
+  id = linearD id
+  D g . D f = D (\ a ->
+    let (b,f') = f a
+        (c,g') = g b
+    in
+      (c, g' . f'))
+  {-# INLINE id #-}
+  {-# INLINE (.) #-}
+
+instance Cartesian (D s) where
+  type Prod (D s) = (:*:)
+  exl = linearD exlF
+  exr = linearD exrF
+  D f &&& D g = D (\ a ->
+    let (b,f') = f a
+        (c,g') = g a
+    in
+      ((b :*: c), f' `forkF` g'))
+  {-# INLINE exl #-}
+  {-# INLINE exr #-}
+  {-# INLINE (&&&) #-}
+
+-- instance Cocartesian (D s) where
+--   type Coprod (D s) = (:*:)
+--   inl = linearD (:*: point 0)
+--   inr = linearD (point 0 :*:)
+--   D f ||| D g = D (\ (a :*: b) ->
+--     let (c,f') = f a
+--         (d,g') = g b
+--     in
+--       (zipWith (+) c d, f' `forkF` g'))
+--   {-# INLINE inl #-}
+--   {-# INLINE inr #-}
+--   {-# INLINE (|||) #-}
+
+#if 0
+
+f :: a s -> (c s, a s -> c s)
+g :: b s -> (c s, b s -> c s)
+
+a :: a s
+b :: b s
+c, d :: c s
+
+f' :: a s -> c s
+g' :: b s -> c s
+
+#endif
