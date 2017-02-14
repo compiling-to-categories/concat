97f394f696496147ac1f8c402cdf26975b4221b8
Conal Elliott <conal@conal.net>
2017-02-13 19:52:16 -0800

    Distributive; experiments with polynomials & optimization.

--- src/ConCat/D.hs
+++ src/ConCat/D.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE TupleSections #-}
 {-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DeriveFoldable #-}
@@ -35,22 +37,24 @@
 
 module ConCat.D where
 
-import Prelude hiding (id,(.),zipWith,curry,uncurry)
+import Prelude hiding (id,(.),const,zipWith,curry,uncurry)
 import qualified Prelude as P
 import Data.Kind
-import GHC.Generics (U1(..),(:*:)(..),(:+:)(..),(:.:)(..))
+import GHC.Generics (U1(..),Par1(..),(:*:)(..),(:+:)(..),(:.:)(..))
+import GHC.Prim (Any)
 import Control.Applicative (liftA2,liftA3)
 import Control.Monad ((<=<))
 import Control.Arrow (arr,Kleisli(..))
 import qualified Control.Arrow as A
 import Control.Monad.State (State,modify,put,get,execState,StateT,evalStateT)
+import Text.Show.Functions () -- for Funk
 
 import Data.Constraint (Dict(..),(:-)(..),refl,trans,(\\))
 import Control.Newtype
 import Data.Pointed
 import Data.Key
 
-import ConCat.Misc (inNew,inNew2,oops,type (+->)(..))
+import ConCat.Misc ((:*),inNew,inNew2,oops,type (+->)(..),R)
 import ConCat.Free.VectorSpace
 import ConCat.Free.LinearRow (lapplyL,OkLF,idL,compL,exlL,exrL,forkL,inlL,inrL,joinL,HasL(..))
 import ConCat.Rep
@@ -114,6 +118,10 @@ dup = id &&& id
 swapP :: forall k a b. (Cartesian k, Ok2 k a b) => Prod k a b `k` Prod k b a
 swapP = exr &&& exl  <+ okProd @k @a @b
 
+-- -- experiment
+-- swapP' :: Cartesian k => Prod k a b `k` Prod k b a
+-- swapP' = ccc (\ (a,b) -> (b,a))
+
 first :: forall k a a' b. (Cartesian k, Ok3 k a b a')
       => (a `k` a') -> (Prod k a b `k` Prod k a' b)
 first = (*** id)
@@ -154,6 +162,10 @@ class (Category k, Ok k (Unit k)) => Terminal k where
   type Unit k :: u
   it :: Ok k a => a `k` Unit k
 
+class (Cartesian k, Cocartesian k) => Distributive k where
+  distl :: Prod k a (Coprod k u v) `k` Coprod k (Prod k a u) (Prod k a v)
+  distr :: Prod k (Coprod k u v) b `k` Coprod k (Prod k u b) (Prod k v b)
+
 class (OkExp k, Cartesian k) => CartesianClosed k where
   type Exp k (a :: u) (b :: u) = (ab :: u) | ab -> a b
   apply   :: forall a b. Ok2 k a b => Prod k (Exp k a b) a `k` b
@@ -169,6 +181,11 @@ class (OkExp k, Cartesian k) => CartesianClosed k where
   {-# MINIMAL curry, (apply | uncurry) #-}
 
 class OkExp k where okExp :: Ok2 k a b :- Ok k (Exp k a b)
+
+class (Category k, Ok k (ConstObj k b)) => ConstCat k b where
+  type ConstObj k b
+  type ConstObj k b = b
+  const :: Ok k a => b -> (a `k` ConstObj k b)
 
 class (Cartesian k, Ok k (BoolOf k)) => BoolCat k where
   type BoolOf k
@@ -262,6 +279,10 @@ instance Terminal (->) where
   type Unit (->) = ()
   it = const ()
 
+instance Distributive (->) where
+  distl (a,uv) = ((a,) +++ (a,)) uv
+  distr (uv,b) = ((,b) +++ (,b)) uv
+
 instance CartesianClosed (->) where
   type Exp (->) a b = a -> b
   apply (f,x) = f x
@@ -269,6 +290,8 @@ instance CartesianClosed (->) where
   uncurry = P.uncurry
 
 instance OkExp (->) where okExp = Sub Dict
+
+instance ConstCat (->) b where const = P.const
 
 instance BoolCat (->) where
   type BoolOf (->) = Bool
@@ -330,6 +353,10 @@ instance OkProd   (Kleisli m) where okProd   = Sub Dict
 instance OkCoprod (Kleisli m) where okCoprod = Sub Dict
 instance OkExp    (Kleisli m) where okExp    = Sub Dict
 
+instance Monad m => Distributive (Kleisli m) where
+  distl = arr distl
+  distr = arr distr
+
 instance Monad m => CartesianClosed (Kleisli m) where
   type Exp (Kleisli m) a b = Kleisli m a b
   apply   = pack (apply . first unpack)
@@ -345,6 +372,148 @@ instance Monad m => CartesianClosed (Kleisli m) where
 --   andC = arr andC
 --   orC  = arr orC
 --   xorC = arr xorC
+
+{--------------------------------------------------------------------
+    Product of categories
+--------------------------------------------------------------------}
+
+infixr 7 :**:
+-- | Product for binary type constructors
+data (k :**: k') (a :: u) (b :: u) = k a b :**: k' a b
+
+instance (Category k, Category k') => Category (k :**: k') where
+  type Ok (k :**: k') a = (Ok k a, Ok k' a)
+  id = id :**: id
+  (g :**: g') . (f :**: f') = (g.f) :**: (g'.f')
+
+#if 0
+
+instance (Cartesian (k :: * -> * -> *), Cartesian (k' :: * -> * -> *)) => Cartesian (k :**: k') where
+  type Prod (k :**: k') a b = (Prod k a b, Prod k' a b)
+  exl = exl :**: exl
+  exr = exr :**: exr
+  (f :**: f') &&& (g :**: g') = (f &&& g) :**: (f' &&& g')
+#if 0
+  (f :**: f') *** (g :**: g') = (f *** g) :**: (f' *** g')
+  dup   = dup   :**: dup
+  swapP = swapP :**: swapP
+  first (f :**: f') = first f :**: first f'
+  second (f :**: f') = second f :**: second f'
+  lassocP = lassocP :**: lassocP
+  rassocP = rassocP :**: rassocP
+#endif
+
+instance (OkProd k, OkProd k') => OkProd (k :**: k') where okProd = Sub Dict
+
+instance (Cocartesian k, Cocartesian k') => Cocartesian (k :**: k') where
+  inl = inl :**: inl
+  inr = inr :**: inr
+  (f :**: f') ||| (g :**: g') = (f ||| g) :**: (f' ||| g')
+#if 0
+  (f :**: f') +++ (g :**: g') = (f +++ g) :**: (f' +++ g')
+  jam = jam :**: jam
+  swapS = swapS :**: swapS
+  left (f :**: f') = left f :**: left f'
+  right (f :**: f') = right f :**: right f'
+  lassocS = lassocS :**: lassocS
+  rassocS = rassocS :**: rassocS
+#endif
+
+-- working on these ones
+
+instance (Terminal k, Terminal k') => Terminal (k :**: k') where
+  -- type Unit (k :**: k) = Prod k (Unit k
+  it = it :**: it
+
+instance (CartesianClosed k, CartesianClosed k') => CartesianClosed (k :**: k') where
+  apply = apply :**: apply
+  -- apply = (apply . exl) :**: (apply . exr)
+  -- apply :: forall a b. (Ok2 k a b, Ok2 k' a b)
+  --       => (k :**: k') ((k :**: k') a b :* a) b
+  -- apply = undefined -- (apply . exl) :**: _
+  curry (f :**: f') = curry f :**: curry f'
+  uncurry (g :**: g') = uncurry g :**: uncurry g'
+
+instance (BoolCat k, BoolCat k') => BoolCat (k :**: k') where
+  notC = notC :**: notC
+  andC = andC :**: andC
+  orC  = orC  :**: orC
+  xorC = xorC :**: xorC
+
+instance (EqCat k a, EqCat k' a) => EqCat (k :**: k') a where
+  equal = equal :**: equal
+  notEqual = notEqual :**: notEqual
+
+instance (NumCat k a, NumCat k' a) => NumCat (k :**: k') a where
+  negateC = negateC :**: negateC
+  addC    = addC    :**: addC
+  subC    = subC    :**: subC
+  mulC    = mulC    :**: mulC
+  powIC   = powIC   :**: powIC
+
+#endif
+
+-- Unit for binary type constructors
+data U2 (a :: u) (b :: u) = U2 deriving (Show)
+
+instance Category U2 where
+  id = U2
+  U2 . U2 = U2
+
+#if 0
+
+type family P (a :: u) (b :: u) = (ab :: u) | ab -> a b
+
+instance Cartesian U2 where
+  -- type Prod U2 a b = Any -- violates injectivity
+  -- type Prod U2 a b = P a b  -- This definition breaks Prod (:-) ?!
+  exl = U2
+  exr = U2
+  U2 &&& U2 = U2
+
+instance OkProd U2 where okProd = Sub Dict
+
+instance Cocartesian U2 where
+  inl = U2
+  inr = U2
+  U2 ||| U2 = U2
+
+instance OkCoprod U2 where okCoprod = Sub Dict
+
+instance Terminal U2 where
+  -- type Unit (U2 :: u -> u -> *) = ??
+  it = U2
+
+instance Distributive U2 where
+  distl = U2
+  distr = U2
+
+instance CartesianClosed U2 where
+  apply = U2
+  curry U2 = U2
+  uncurry U2 = U2
+
+instance OkExp U2 where okExp = Sub Dict
+
+instance BoolCat U2 where
+  type BoolOf U2 = ()  -- arbitrary
+  notC = U2
+  andC = U2
+  orC  = U2
+  xorC = U2
+
+instance EqCat U2 a where
+  equal = U2
+  notEqual = U2
+
+instance NumCat U2 a where
+  negateC = U2
+  addC    = U2
+  subC    = U2
+  mulC    = U2
+  powIC   = U2
+
+#endif
 
 {--------------------------------------------------------------------
     Constraint entailment
@@ -582,7 +751,7 @@ instance Num s => FunctorC (Linear s) (Arg s) (LM s) where
 --------------------------------------------------------------------}
 
 -- | Differentiable function on vector space with field s
-data DF s a b = D { unD :: a s -> (b s, LM s a b) }
+data DF s a b = D { unD :: a s -> b s :* LM s a b }
 
 -- TODO: try a more functorish representation: (a :->: b :*: (a :->: b))
 
@@ -773,6 +942,7 @@ instance CartesianClosedFunctor (Standardize s) (->) (->) where preserveExp    =
 {--------------------------------------------------------------------
     Memoization
 --------------------------------------------------------------------}
+
 class HasTrie a where
   type Trie a :: * -> *
   toTrie :: (a -> b) -> Trie a b
@@ -864,7 +1034,7 @@ instance FunctorC UnMemo (:->:) (->) where
   (%) UnMemo = unMemo
 
 {--------------------------------------------------------------------
-    Free vector spaces
+    Free vector spaces as functions
 --------------------------------------------------------------------}
 
 class Enumerable a where enumerate :: [a]
@@ -897,3 +1067,303 @@ instance Num s => Category (FL s) where
 -- instance Num s => Cartesian (FL s) where
 --   type Prod (FL s) = (,)
 --   exl = FL _
+
+{--------------------------------------------------------------------
+    Affine functions
+--------------------------------------------------------------------}
+
+data Affine s a b = Affine (b s) (LM s a b)
+
+linearA :: (Num s, Ok2 (LM s) a b) => LM s a b -> Affine s a b
+linearA = Affine zeroV
+
+instance Num s => Category (Affine s) where
+  type Ok (Affine s) a = OkLF a
+  id = linearA id
+  Affine b g . Affine a f = Affine (b ^+^ lapply g a) (g . f)
+
+instance Num s => Cartesian (Affine s) where
+  type Prod (Affine s) a b = a :*: b
+  exl = linearA exl
+  exr = linearA exr
+  Affine b f &&& Affine c g = Affine (b :*: c) (f &&& g)
+
+instance OkProd (Affine s) where okProd = Sub Dict
+
+instance Num s => Cocartesian (Affine s) where
+  type Coprod (Affine s) a b = a :*: b
+  inl = linearA inl
+  inr = linearA inr
+  Affine c f ||| Affine d g = Affine (c ^+^ d) (f ||| g)
+
+instance OkCoprod (Affine s) where okCoprod = Sub Dict
+
+-- ...
+
+{--------------------------------------------------------------------
+    Polynomials
+--------------------------------------------------------------------}
+
+#if 0
+
+newtype Poly a b = Poly [Double]
+
+instance Category (Poly :: * -> * -> *) where
+  type Ok Poly a = a ~ Double
+  id = undefined
+  (.) = undefined
+
+instance Cartesian Poly where ...
+
+#endif
+
+{--------------------------------------------------------------------
+    Functions with constant folding
+--------------------------------------------------------------------}
+
+data Funk a b = FunConst b | Fun (a -> b)
+
+-- TODO: generalize from (->)
+
+instance Show b => Show (Funk a b) where
+  showsPrec p (FunConst b) = showsApp "const" b p
+                             -- showsApp "FunConst" b p
+  showsPrec p (Fun f)      = showsPrec p f
+                             -- showsApp "Fun"      f p
+
+showsApp :: Show b => String -> b -> Int -> ShowS
+showsApp nm b p = showString (nm ++ " ") . showParen (p >= 9) (showsPrec 10 b)
+
+applyFunk :: Funk a b -> (a -> b)
+applyFunk (FunConst b) = const b
+applyFunk (Fun f)      = f
+
+instance Category Funk where
+  id = Fun id
+  Fun g . Fun f = Fun (g . f)
+  g . FunConst a = FunConst (applyFunk g a)
+  FunConst b . _ =  FunConst b
+
+-- g . const a == const (g a)
+-- const b . f == const b
+
+instance Cartesian Funk where
+  type Prod Funk a b = (a,b)
+  exl = Fun exl
+  exr = Fun exr
+  FunConst c &&& FunConst d = FunConst (c,d)
+  f &&& g = Fun (applyFunk f &&& applyFunk g)
+
+-- const c &&& const d == const (c,d)
+
+instance OkProd Funk where okProd = Sub Dict
+
+-- What if *part* of the structure is known to be constant?
+-- Possibly a more interesting product type.
+-- If we apply exl to a left-constant pair, we get a constant function.
+
+instance Cocartesian Funk where
+  type Coprod Funk a b = Either a b
+  inl = Fun inl
+  inr = Fun inr
+  f ||| g =  Fun (applyFunk f ||| applyFunk g)
+
+instance OkCoprod Funk where okCoprod = Sub Dict
+
+instance ConstCat Funk b where const = FunConst
+
+-- (|||) :: (a `k` c) -> (b `k` c) -> ((a + b) `k` c)
+
+-- (f ||| g) (Left  a) == f a
+-- (f ||| g) (Right b) == g b
+
+-- const f
+
+-- Then first-order: constant, affine, or general. Other variants?
+
+c1 :: Funk Int Int
+c1 = const 3
+
+c2 :: Funk Int Int
+c2 = Fun succ
+
+c3 :: Funk Int Int
+c3 = c2 . c1
+
+c4 :: Funk Int Int
+c4 = c1 . c2
+
+
+infixr 3 :&&&, :***
+infixr 2 :+++, :|||
+
+data Funky :: * -> * -> * where
+  AllConst  :: {- Eq b => -} b -> Funky a b
+  Id :: Funky a a
+  (:&&&) :: Funky a c -> Funky a d -> Funky a (c,d)
+  CompExl :: Funky a c -> Funky (a,b) c
+  CompExr :: Funky b c -> Funky (a,b) c
+  (:|||) :: Funky a c -> Funky b c -> Funky (Either a b) c
+  InlComp :: Funky a c -> Funky a (Either c d)
+  InrComp :: Funky a d -> Funky a (Either c d)
+  Fun' :: (a -> b) -> Funky a b
+
+-- TODO: generalize from (->): Funky k a b
+
+pattern (:***) :: Funky a c -> Funky b d -> Funky (a,b) (c,d)
+pattern f :*** g = CompExl f :&&& CompExr g
+
+pattern Cross :: Funky a c -> Funky b d -> Funky (a,b) (c,d)
+pattern Cross f g = CompExl f :&&& CompExr g
+
+pattern (:+++) :: Funky a c -> Funky b d -> Funky (Either a b) (Either c d)
+pattern f :+++ g = InlComp f :||| InrComp g
+
+-- TODO: "Fork", "Join", "Cross", "Plus" --> ":&&&", ":|||", ":***", ":+++"
+
+knownEq :: Funky a b -> Funky a b -> Bool
+-- knownEq (AllConst b) (AllConst b') = b == b'  -- needs Eq
+knownEq Id           Id            = True
+knownEq (f :&&& g)   (f' :&&& g')  = knownEq f f' && knownEq g g'
+knownEq (CompExl f)  (CompExl g)   = knownEq f g
+knownEq (CompExr f)  (CompExr g)   = knownEq f g
+knownEq (f :||| g)   (f' :||| g')  = knownEq f f' && knownEq g g'
+knownEq (InlComp f)  (InlComp g)   = knownEq f g
+knownEq (InrComp f)  (InrComp g)   = knownEq f g
+knownEq _            _             = False
+
+-- -- Convert to Syn
+-- instance Show b => Show (Funky a b) where
+
+applyFunky :: Funky a b -> (a -> b)
+applyFunky (AllConst b) = const b
+applyFunky Id           = id
+applyFunky (CompExl f)  = applyFunky f . exl
+applyFunky (CompExr g)  = applyFunky g . exr
+applyFunky (f :&&& g)   = applyFunky f &&& applyFunky g
+applyFunky (f :||| g)   = applyFunky f ||| applyFunky g
+applyFunky (InlComp f)  = inl . applyFunky f
+applyFunky (InrComp g)  = inr . applyFunky g
+applyFunky (Fun' h)     = h
+
+instance Category Funky where
+  id = Fun' id
+  AllConst b . _ = AllConst b
+  Id . f = f
+  CompExl h . (f :&&& _) = h . f
+  CompExr h . (_ :&&& g) = h . g
+  (AllConst c :&&& g) . f = AllConst c &&& g . f
+  (g  :&&& AllConst c) . f = (g . f) &&& AllConst c
+  (CompExl f :&&& CompExr g) . (h :&&& k) = (f . h) &&& (g . k)
+  -- (f :*** g) . (h :&&& k) = ((f . h) &&& (g . k))  -- GHC bug?
+  InlComp g . f = InlComp (g . f)
+  InrComp g . f = InrComp (g . f)
+  g . AllConst a = AllConst (applyFunky g a)
+  g . Id = g
+  g . CompExl f = CompExl (g . f)
+  g . CompExr f = CompExr (g . f)
+  g . f = Fun' (applyFunky g . applyFunky f)
+#if 0
+#endif
+
+-- f :: F p r
+-- g :: F q s
+-- h :: F u p
+-- k :: F u q
+
+-- f &&& g :: F 
+
+
+-- (g &&& h) . f
+
+--  (h . exl) . (f &&& g) == h . f
+--  (h . exr) . (f &&& g) == h . g
+
+-- (f *** g) . (h &&& k) == (f . h) &&& (g . k)
+
+-- (f . exl &&& g . exr) . (h &&& k) == (f . h) &&& (g . k)
+
+-- (const c &&& g) . f == const c &&& (g . f)
+-- (g &&& const d) . f == (g . f) &&& const d
+
+-- (inl . g) . f == inl . (g . f)
+-- (inr . g) . f == inr . (g . f)
+
+-- g . const a = const (g a)
+-- g . (f . exl) = (g . f) . exl
+-- g . (f . exr) = (g . f) . exr
+
+-- g . (const c &&& f) == ... -- precompute! what do I need?
+-- g . (f &&& const c) == ... -- precompute! what do I need?
+-- g . (inl . f) == ??
+
+-- general partial evaluation?
+
+instance Cartesian Funky where
+  type Prod Funky a b = (a,b)
+  exl = CompExl id
+  exr = CompExr id
+  AllConst c &&& AllConst d = AllConst (c,d)
+  CompExl Id &&& CompExr Id = Id  -- generalize to other detectably equal terms?
+  -- CompExl f &&& CompExr f' | knownEq f f' = f
+  f &&& g = f :&&& g
+
+instance OkProd Funky where okProd = Sub Dict
+
+-- exl &&& exr == id
+
+-- (f &&& g) . h == (f . h) &&& (g . h)
+
+-- exl . h &&& exr . h == h
+
+-- f . exl . h &&& g . exr . h == (f *** g) . (exl . h &&& exr . h) == (f *** g) . h === (f . exl &&& g . exr) . h
+
+-- exl &&& exr
+
+
+-- Idea: Represent Funky'' a b as forall ...
+
+{--------------------------------------------------------------------
+    Polynomials
+--------------------------------------------------------------------}
+
+type OkPoly b = (Pointed b, Functor b, Zip b)  -- OkLF b ?
+
+-- Poly R = []
+-- Poly (a,b) = Poly a :.: Poly b
+
+class Functor (PolyF a) => HasPoly a where
+  type PolyF (a :: * -> *) :: * -> *
+  evalPoly :: (OkPoly c, Num s) => PolyF a (c s) -> a s -> c s
+
+instance HasPoly U1 where
+  type PolyF U1 = U1
+  evalPoly :: (OkPoly c, Num s) => U1 (c s) -> U1 s -> c s
+  evalPoly U1 U1 = zeroV
+
+instance HasPoly Par1 where
+  type PolyF Par1 = []
+  evalPoly :: (OkPoly c, Num s) => [c s] -> Par1 s -> c s
+  evalPoly c (Par1 s) = foldr (\ w z -> w ^+^ s *^ z) zeroV c
+
+instance (HasPoly a, HasPoly b) => HasPoly (a :*: b) where
+  type PolyF (a :*: b) = PolyF a :.: PolyF b
+  evalPoly :: (OkPoly c, Num s) => (PolyF a :.: PolyF b) (c s) -> (a :*: b) s -> c s
+  evalPoly (Comp1 w) (a :*: b) = evalPoly (flip evalPoly b <$> w) a
+
+#if 0
+  
+w ::: PolyF a (PolyF b (c s))
+a :: a s
+b :: b s
+
+flip evalPoly b :: PolyF b (c s) -> c s
+flip evalPoly b <$> w :: PolyF a (c s)
+evalPoly (flip evalPoly b <$> w) a :: c s
+
+#endif
+
+data Poly a b = forall s. Num s => Poly (PolyF a (b s))
+
+-- instance Category Poly where
+--   id = ...
