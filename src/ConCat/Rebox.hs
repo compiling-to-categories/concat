bf52cdceeb77c2c8395ddaf46af745eaebaa8b65
Conal Elliott <conal@conal.net>
2017-03-28 11:36:37 -0700

    float & double division

--- src/ConCat/Rebox.hs
+++ src/ConCat/Rebox.hs
@@ -89,4 +89,70 @@ boxIB i = tagToEnum# i
 --                      }
 --       Orig lhs: case /## u v of wild_00 { __DEFAULT -> boxD wild_00 }
 
+-- /## 1.0## (cosDouble# x)
+
+"boxD /" forall u v. u /## v            = unD (divideC (boxD u, boxD v))
+"boxF /" forall u v. u `divideFloat#` v = unF (divideC (boxF u, boxF v))
+
+-- TODO: Maybe change all the the reboxing rules to this style. Or maybe not,
+-- since it's not driven by ccc, and hence could easily degrade all numeric
+-- performance.
+
  #-}
+
+unF :: Float -> Float#
+unF (F# f) = f
+-- {-# INLINE [0] unF #-}
+
+unD :: Double -> Double#
+unD (D# d) = d
+-- {-# INLINE [0] unD #-}
+
+-- The float/double division reboxing scheme works without the INLINE pragmas or
+-- with INLINE [1-4], but gives the following warning with INLINE [0]:
+
+{-
+
+ghc: panic! (the 'impossible' happened)
+  (GHC version 8.0.2 for x86_64-apple-darwin):
+	ccc post-transfo check. Lint
+      [RHS of wild1_agZn :: Double#]
+      The type of this binder is primitive: wild1_agZn
+      Binder's type: Double#
+  ccc
+    @ Syn
+    @ (R, R2)
+    @ Bool
+    (\ (x_eta_Bh :: (R, R2)) ->
+       case unD
+              (divideC
+                 @ (->)
+                 @ Double
+                 ($fFractionalCat(->)a @ Double $fFractionalDouble)
+                 (boxD 1.0##,
+                  cosC
+                    @ (->)
+                    @ Double
+                    ($fFloatingCat(->)a @ Double $fFloatingDouble)
+                    (exl
+                       @ (->)
+                       @ R
+                       @ R2
+                       $fProductCat(->)
+                       ...
+                       x_eta_Bh)))
+       of wild1_agZn { __DEFAULT ->
+
+-}
+
+-- When I turn off lintSteps in ConCat.Plugin, we get into an infinite
+-- unfolding/reboxing loop. I tried the following rules
+-- 
+-- "D# . unD" forall u. D# (unD u) = u
+-- "F# . unF" forall u. F# (unF u) = u
+-- 
+-- but
+-- 
+--     RULE left-hand side too complicated to desugar
+--       Optimised lhs: case unD u of wild_00 { __DEFAULT ->
+--                      GHC.Types.D# wild_00 }
