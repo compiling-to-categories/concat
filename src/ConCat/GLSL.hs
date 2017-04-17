dbc995ac61dad6622bef258ca7e6e103205f8b37
Conal Elliott <conal@conal.net>
2017-04-16 20:33:19 -0700

    Back in sync with ConCat.Circuit

--- src/ConCat/GLSL.hs
+++ src/ConCat/GLSL.hs
@@ -15,9 +15,8 @@
 module ConCat.GLSL (CAnim,genGlsl) where
 
 import Control.Monad (when)
-import Data.Ord (comparing)
 import Data.Char (isAlphaNum)
-import Data.List (sortBy)
+import Data.List (sort)
 import qualified Data.Set as S
 import qualified Data.Map as M
 import System.Directory (createDirectoryIfMissing)
@@ -30,7 +29,7 @@ import Language.GLSL.Pretty ()
 import Language.GLSL.Parser hiding (parse)
 
 import ConCat.Misc ((:*),R)
-import ConCat.Circuit (Prim(..),CompS(..),CompId,Bus(..),busTy,(:>), mkGraph)
+import ConCat.Circuit (Comp(..),Bus(..),busTy,(:>),mkGraph,pattern CompS)
 import qualified ConCat.Circuit as C
 
 type CAnim = R :* (R :* R) :> Bool
@@ -40,13 +39,12 @@ showGraph = False -- True
 
 genGlsl :: String -> CAnim -> IO ()
 genGlsl name circ =
-  do when showGraph $ putStrLn $ "genGlsl: Graph " ++ show g
+  do when showGraph $ putStrLn $ "genGlsl: Graph " ++ show comps
      createDirectoryIfMissing False outDir
      writeFile (outDir++"/"++name++".frag") (prettyShow fundef ++ "\n")
  where
-   g@(compDepths,_report) = mkGraph circ
-   comps = sortBy (comparing C.compId) (M.keys compDepths)
-   fundef = fromComps' (tweakName name) comps
+   comps = sort (mkGraph circ)
+   fundef = fromComps (tweakName name) comps
    outDir = "out"
    tweakName = map tweakChar
    tweakChar c | isAlphaNum c = c
@@ -59,41 +57,37 @@ constExpr C.Float  = FloatConstant       . read
 constExpr C.Double = FloatConstant       . read
 constExpr ty = error ("ConCat.GLSL.constExpr: unexpected literal type: " ++ show ty)
 
-pattern CompS' :: CompId -> String -> [Bus] -> [Bus] -> CompS
-pattern CompS' n name ins outs <- CompS n (Prim name) ins outs _
-
-fromComps' :: String -> [CompS] -> ExternalDeclaration
--- fromComps' _ comps | trace ("fromComps' " ++ show comps) False = undefined
-fromComps' name comps =
-  funDef Bool name (paramDecl <$> inputs)
-         (map (uncurry initBus) assignments
-          ++ [Return (Just (bindings M.! res))])
- where
-   (CompS' _ "In" [] inputs,mid, CompS' _ "Out" [res] _) = splitComps comps
-   (bindings, assignments) = accumComps (uses mid) mid
+fromComps :: String -> [Comp] -> ExternalDeclaration
+-- fromComps _ comps | trace ("fromComps " ++ show comps) False = undefined
+fromComps name comps
+  | (CompS _ "In" [] inputs,mid, CompS _ "Out" [res] _) <- splitComps comps
+  , let (bindings, assignments) = accumComps (uses mid) mid
+  = funDef Bool name (paramDecl <$> inputs)
+           (map (uncurry initBus) assignments
+            ++ [Return (Just (bindings M.! res))])
 
 -- Count uses of each output
-uses :: [CompS] -> M.Map Bus Int
+uses :: [Comp] -> M.Map Bus Int
 uses = M.unionsWith (+) . map uses1
 
 -- Uses map for a single component
-uses1 :: CompS -> M.Map Bus Int
-uses1 (CompS _ _ ins _ _) = M.unionsWith (+) (flip M.singleton 1 <$> ins)
+uses1 :: Comp -> M.Map Bus Int
+uses1 (CompS _ _ ins _) = M.unionsWith (+) (flip M.singleton 1 <$> ins)
 
 nestExpressions :: Bool
 nestExpressions = True -- False
 
 -- Given usage counts, generate delayed bindings and assignments
-accumComps :: M.Map Bus Int -> [CompS] -> (M.Map Bus Expr, [(Bus,Expr)])
+accumComps :: M.Map Bus Int -> [Comp] -> (M.Map Bus Expr, [(Bus,Expr)])
 -- accumComps counts | trace ("accumComps: counts = " ++ show counts) False = undefined
 accumComps counts = go M.empty
  where
    -- Generate bindings for outputs used more than once,
    -- and accumulate a map of the others.
-   go :: M.Map Bus Expr -> [CompS] -> (M.Map Bus Expr, [(Bus,Expr)])
+   go :: M.Map Bus Expr -> [Comp] -> (M.Map Bus Expr, [(Bus,Expr)])
    -- go saved comps | trace ("accumComps/go " ++ show saved ++ " " ++ show comps) False = undefined
    go saved [] = (saved, [])
-   go saved (c@(CompS _ _ _ [o] _) : comps) 
+   go saved (c@(CompS _ _ _ [o]) : comps) 
      | Just n <- M.lookup o counts, (n > 1 || not nestExpressions) =
          let (saved',bindings') = go saved comps in
            (saved', (o,e) : bindings')
@@ -102,9 +96,9 @@ accumComps counts = go M.empty
       e = compExpr saved c
    go _ c = error ("ConCat.GLSL.accumComps: oops: " ++ show c)
 
-compExpr :: M.Map Bus Expr -> CompS -> Expr
-compExpr _ (CompS' _ str [] [Bus _ _ ty]) = constExpr ty str
-compExpr saved (CompS' _ prim ins _) = app prim (inExpr <$> ins)
+compExpr :: M.Map Bus Expr -> Comp -> Expr
+compExpr _ (CompS _ str [] [Bus _ _ ty]) = constExpr ty str
+compExpr saved (CompS _ prim ins _) = app prim (inExpr <$> ins)
  where
    inExpr :: Bus -> Expr
    inExpr b | Just e <- M.lookup b saved = e
@@ -134,28 +128,20 @@ varName b = error ("ConCat.GLSL.varName unexpected " ++ show b)
 -- primitives.
 
 app :: String -> [Expr] -> Expr
-app "¬"      [e]     = UnaryNot e
-app "∧"      [e1,e2] = And e1 e2
+app "not"      [e]     = UnaryNot e
 app "&&"     [e1,e2] = And e1 e2
-app "∨"      [e1,e2] = Or  e1 e2
 app "||"     [e1,e2] = Or  e1 e2
 app "<"      [e1,e2] = Lt  e1 e2
 app ">"      [e1,e2] = Gt  e1 e2
-app "≤"      [e1,e2] = Lte e1 e2
 app "<="     [e1,e2] = Lte e1 e2
-app "≥"      [e1,e2] = Gte e1 e2
 app ">="     [e1,e2] = Gte e1 e2
-app "≡"      [e1,e2] = Equ e1 e2
 app "=="     [e1,e2] = Equ e1 e2
-app "≢"      [e1,e2] = Neq e1 e2
 app "/="     [e1,e2] = Neq e1 e2
 app "negate" [e]     = UnaryNegate e
 app "+"      [e1,e2] = Add e1 e2
 app "-"      [e1,e2] = Sub e1 e2
 app "−"      [e1,e2] = Sub e1 e2
-app "×"      [e1,e2] = Mul e1 e2
 app "*"      [e1,e2] = Mul e1 e2
-app "÷"      [e1,e2] = Div e1 e2
 app "/"      [e1,e2] = Div e1 e2
 app "mod"    [e1,e2] = Mod e1 e2
 app fun args | fun `S.member` knownFuncs = funcall fun args
@@ -168,9 +154,9 @@ bToE :: Bus -> Expr
 bToE = Variable . varName
 
 -- Extract input, middle, output components. 
-splitComps :: [CompS] -> (CompS,[CompS],CompS)
-splitComps (i@(CompS' 0 "In" [] _)
-            : (unsnoc -> (mid,o@(CompS' _ "Out" _ [])))) = (i,mid,o)
+splitComps :: [Comp] -> (Comp,[Comp],Comp)
+splitComps (i@(CompS _ "In" [] _)
+            : (unsnoc -> (mid,o@(CompS _ "Out" _ [])))) = (i,mid,o)
 splitComps comps = error ("ConCat.GLSL.splitComps: Oops: " ++ show comps)
 
 unsnoc :: [a] -> ([a],a)
