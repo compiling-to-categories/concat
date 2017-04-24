906a484f4179077d930b0d4a3c72d81992b0b6db
Conal Elliott <conal@conal.net>
2017-04-24 16:35:40 -0700

    simplify: no more int indices for now.

--- src/ConCat/Fun.hs
+++ src/ConCat/Fun.hs
@@ -1,31 +1,65 @@
 {-# LANGUAGE InstanceSigs #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE CPP #-}
-{-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DefaultSignatures #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE ViewPatterns #-}
-
-{-# LANGUAGE UndecidableInstances #-}  -- TEMP
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 
 {-# OPTIONS_GHC -Wall #-}
-{-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
-
+{-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
+{-# OPTIONS_GHC -Wredundant-constraints #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
 
--- | 
+#include "AbsTy.inc"
+AbsTyPragmas
+
+#define CustomEnum
+
+-- | Another go at safe array wrappers
 
 module ConCat.Fun where
 
-import Data.Monoid ((<>))
-import Data.Foldable
-import Control.Applicative (liftA2)
-import Control.Arrow ((***),(|||))
+#ifdef CustomEnum
+import Prelude hiding (Enum(..))
+#endif
 
-import ConCat.Misc ((:+),(:*),(<~),Unop,Binop)
+import Control.Arrow ((|||),(***))
+import GHC.Generics
+  (Generic1(..),U1(..),Par1(..),(:*:)(..),(:.:)(..),M1(..),K1(..))
+import Data.Tuple (swap)
+import Data.Void
+import Data.Monoid ((<>),Sum(..))
+import Data.Foldable (Foldable(..),toList)
+import Data.Traversable (mapAccumL)
+
+import Control.Newtype
+
+import ConCat.Misc ((:*),(:+), (<~), inNew, inNew2)
+import ConCat.Rep
+import ConCat.AltCat (Arr,mkArr,arrAt,divC,modC)
+
+AbsTyImports
+
+{--------------------------------------------------------------------
+    Foldable functions
+--------------------------------------------------------------------}
+
+instance Foldable ((->) Void) where
+  foldMap _f _h = mempty
 
 instance Foldable ((->) ()) where
   foldMap f h = f (h ())
@@ -125,234 +159,48 @@ uncurry <$> traverse (traverse f) (curry h) :: g (a :* b -> c)
 
 #endif
 
+instance Newtype (Arr a b) where
+  type O (Arr a b) = a -> b
+  pack   = mkArr
+  unpack = curry arrAt
+  {-# INLINE pack #-}
+  {-# INLINE unpack #-}
 
--- Note that we can avoid orphans by newtype-wrapping function types:
+instance Functor (Arr a) where
+  fmap = inNew . fmap
+  {-# INLINE fmap #-}
 
-newtype Fun k v = Fun (k -> v) deriving (Functor,Applicative,Monad,Monoid)
+instance Applicative (Arr a) where
+  pure = pack . pure
+  (<*>) = inNew2 (<*>)
+  {-# INLINE pure #-}
+  {-# INLINE (<*>) #-}
 
--- Give (non-orphan instances) for other classes:
+instance Foldable ((->) a) => Foldable (Arr a) where
+  foldMap f = foldMap f . unpack
+  {-# INLINE foldMap #-}
 
-instance Foldable (Fun ()) where
-  foldMap f (Fun h) = f (h ())
+instance {-# overlapping #-} (Foldable (Arr a), Foldable ((->) b))
+      => Foldable (Arr (a :* b)) where
+  foldMap f arr = fold (mkArr (fmap (foldMap f) (curry (curry arrAt arr))))
+  {-# INLINE foldMap #-}
 
-instance Foldable (Fun Bool) where
-  foldMap f (Fun h) = f (h False) <> f (h True)
-
--- instance (Foldable (Fun a), Foldable (Fun b)) => Foldable (Fun (a :+ b)) where
---   foldMap f (Fun h) = foldMap f (Fun (h . Left)) <> foldMap f (Fun (h . Right))
-
--- instance (Foldable (Fun a), Foldable (Fun b)) => Foldable (Fun (a :* b)) where
---   foldMap f (Fun h) = (foldMap . foldMap) f (Fun (Fun <$> curry h))
-
--- Alternatively,
-
-foldMapFun :: (Foldable (Fun a), Monoid m) => (z -> m) -> (a -> z) -> m
-foldMapFun f h = foldMap f (Fun h)
-
-instance (Foldable (Fun a), Foldable (Fun b)) => Foldable (Fun (a :+ b)) where
-  foldMap f (Fun h) = foldMapFun f (h . Left) <> foldMapFun f (h . Right)
-
-instance (Foldable (Fun a), Foldable (Fun b)) => Foldable (Fun (a :* b)) where
-  foldMap f (Fun h) = (foldMapFun . foldMapFun) f (curry h)
-
-
--- Type cardinality.
--- If useful, generalize to card :: Num n => n
-class HasCard a where card :: Num n => n
-                      -- card :: Int
-
-instance HasCard ()   where card = 1
-
-instance HasCard Bool where card = 2
-
-instance (HasCard a, HasCard b) => HasCard (a :+ b) where
-  card = card @a + card @b
-
-instance (HasCard a, HasCard b) => HasCard (a :* b) where
-  card = card @a * card @b
-
--- | Convert to and from the ordinal position within a type.
-class Ordable a where
-  toOrd :: Num n => a -> n
-  unOrd :: (Integral n, Show n, Ord n) => n -> a
-
-instance Ordable () where
-  toOrd () = 0
-  unOrd 0 = ()
-  unOrd i = error ("unOrd @(): bad index: " ++ show i)
-
-instance Ordable Bool where
-  -- toOrd b = if b then 1 else 0
-  toOrd = bool 0 1
-  unOrd 0 = False
-  unOrd 1 = True
-  unOrd i = error ("unOrd @Bool: bad index: " ++ show i)
-
-instance (Ordable a, HasCard a, Ordable b) => Ordable (a :+ b) where
-  toOrd (Left  a) = toOrd a
-  toOrd (Right b) = card @a + toOrd b
-  unOrd i | i < card @a = Left  (unOrd i)
-          | otherwise   = Right (unOrd (i - card @a))
-
-instance (Ordable a, HasCard b, Ordable b) => Ordable (a :* b) where
-  toOrd (a,b) = toOrd a * card @b + toOrd b
-  -- unOrd i = (unOrd ia, unOrd ib) where (ia,ib) = i `divMod` card @b
-  -- unOrd ((`divMod` card @b) -> (ia,ib)) = (unOrd ia, unOrd ib)
-  unOrd = (unOrd *** unOrd) . (`divMod` card @b)
-
--- Maybe use `Enum` instead:
-
-instance (Enum a, HasCard a, Enum b) => Enum (a :+ b) where
-  -- fromEnum (Left  a) = fromEnum a
-  -- fromEnum (Right b) = card @a + fromEnum b
-  fromEnum = fromEnum ||| (card @a +) . fromEnum
-  toEnum i | i < card @a = Left  (toEnum i)
-           | otherwise   = Right (toEnum (i - card @a))
-
-instance (Enum a, HasCard b, Enum b) => Enum (a :* b) where
-  fromEnum (a,b) = fromEnum a * card @b + fromEnum b
-  -- toEnum i = (toEnum ia, toEnum ib) where (ia,ib) = i `divMod` card @b
-  -- toEnum ((`divMod` card @b) -> (ia,ib)) = (toEnum ia, toEnum ib)
-  toEnum = (toEnum *** toEnum) . (`divMod` card @b)
-
--- I like Enum here, despite orphan instances.
+-- f :: c -> m
+-- arr :: Arr (a :* b) c
+-- curry arrAt arr :: a :* b -> c
+-- curry (curry arrAt arr) :: a -> b -> c
+-- fmap (foldMap f) (curry (curry arrAt arr)) :: a -> m
+-- mkArr (fmap (foldMap f) (curry (curry arrAt arr))) :: Arr a m
+-- fold (mkArr (fmap (foldMap f) (curry (curry arrAt arr)))) :: m
 
 
-newtype Arr a = Arr (Int -> a) deriving (Monoid,Functor,Applicative,Monad)
 
-unFun :: Enum k => Fun k a -> Arr a
-unFun (Fun f) = Arr (f . toEnum)
+instance Traversable ((->) a) => Traversable (Arr a) where
+  traverse f = fmap pack . traverse f . unpack
+  {-# INLINE traverse #-}
 
-toFun :: Enum k => Arr a -> Fun k a
-toFun (Arr h) = Fun (h . fromEnum)
+lscan :: (Traversable f, Monoid a) => f a -> f a :* a
+lscan = swap . mapAccumL (\ acc a -> (acc <> a,acc)) mempty
 
-onFun :: Enum k => (Fun k a -> Fun k b) -> (Arr a -> Arr b)
-onFun = unFun <~ toFun
--- onFun h = unFun . h . toFun
--- onFun h f = unFun (h (toFun f))
-
-onFun2 :: Enum k => (Fun k a -> Fun k b -> Fun k c) -> (Arr a -> Arr b -> Arr c)
-onFun2 = onFun <~ toFun
-
-
-type R = Double
-
-sqr :: Num a => Unop a
-sqr a = a * a
-
-t1 :: forall k a. (Enum k, Num a) => Unop (Arr a)
-t1 = onFun @k (fmap sqr)
-
-t2 :: forall k a. (Enum k, Num a) => Binop (Arr a)
-t2 = onFun2 @k (liftA2 (+))
-
-t3 :: forall k a. (Foldable (Fun k), Enum k, Num a) => Arr a -> a
-t3 = sum . toFun @k
-
-
--- Function from k to v, represented as an "array"
-newtype TArr k v = TArr (Int -> v) deriving (Monoid,Functor,Applicative,Monad)
-
-unTArr :: Enum k => TArr k v -> (k -> v)
-unTArr (TArr h) = h . fromEnum
-
-toTArr :: Enum k => (k -> v) -> TArr k v   -- inverse of unTArr
-toTArr h = TArr (h . toEnum)
-
--- -- Specification: unTarr is a `Foldable` homomorphism.
--- instance (Foldable ((->) k), Enum k) => Foldable (TArr k) where
---   fold = fold . unTArr
---   -- foldMap f = fold . fmap f
---   -- foldMap f = fold . unTArr . fmap f
---   -- foldMap f = fold . fmap f . unTArr
---   foldMap f = foldMap f . unTArr
-
-#if 0
-  foldMap f (TArr h)
-== fold (fmap f (TArr h))
-== fold (TArr (fmap f h))
-== fold (TArr (f . h))
-#endif
-
-instance Foldable (TArr ()) where
-  fold (TArr h) = h 0
-  foldMap f (TArr h) = f (h 0)
-
-#if 0
-  fold (TArr h)
-== fold (unTArr (TArr h))
-== fold (h . fromEnum @())
-== fold (\ () -> h (fromEnum ()))
-== fold (\ () -> h 0)
-== (\ () -> h 0) ()
-== h 0
-
-  foldMap f (TArr h)
-== foldMap f (unTArr (TArr h))
-== foldMap f (h . fromEnum @())
-== foldMap f (\ () -> h (fromEnum ()))
-== foldMap f (\ () -> h 0)
-== f ((\ () -> h 0) ())
-== f (h 0)
-#endif
-
-instance Foldable (TArr Bool) where
-  foldMap f (TArr h) = f (h 0) <> f (h 1)
-
-#if 0
-  fold (TArr h)
-== fold (unTArr (TArr h))
-== fold (h . fromEnum @Bool)
-== h (fromEnum False) <> h (fromEnum True)
-== h 0 <> h 1
-
-  foldMap f (TArr h)
-== foldMap f (h . fromEnum @Bool)
-== f (h 0) <> f (h 1)
-
-  foldMap f (TArr h)
-== fold (TArr (f . h))
-== f (h 0) <> f (h 1)
-#endif
-
-instance (Foldable (TArr a), HasCard a, Foldable (TArr b))
-      => Foldable (TArr (a :+ b)) where
-  foldMap f (TArr h) = fold (TArr @(a :+ b) (f . h))
-  fold (TArr h) = fold (TArr @a h) <> fold (TArr @b (h . (card @a +)))
-
-#if 0
-
-  fold (TArr @(a :+ b) h)
-== fold (unTArr (TArr h))
-== fold (h . fromEnum @(a :+ b))
-== fold (h . (fromEnum @a ||| (card @a +) . fromEnum @b))
-== fold (h . (fromEnum @a ||| (card @a +) . fromEnum @b) . Left ) <>
-   fold (h . (fromEnum @a ||| (card @a +) . fromEnum @b) . Right)
-== fold (h . fromEnum @a) <> fold (h . (card @a +) . fromEnum @b)
-== fold (TArr @a h) <> fold (TArr @b (h . (card @a +)))
-
-#endif
-
-instance (Foldable (TArr a), Foldable (TArr b), HasCard b)
-      => Foldable (TArr (a :* b)) where
-  foldMap f (TArr h) = fold (TArr @(a :* b) (f . h))
-  fold (TArr h) = 
-    fold (TArr @a (\ ia -> let n = ia * card @b in fold (TArr @b (h . (n +)))))
-
-#if 0
-
-  fold (TArr @(a :* b) h)
-== fold (unTArr (TArr h))
-== fold (h . fromEnum @(a :* b))
-== fold (fold . curry (h . (\ (a,b) -> fromEnum a * card @b + fromEnum b)))
-== fold (fold . curry (\ (a,b) -> h (fromEnum a * card @b + fromEnum b)))
-== fold (fold . \ a b -> h (fromEnum a * card @b + fromEnum b))
-== fold (\ a -> let n = fromEnum a * card @b in fold (\ b -> h (n + fromEnum b)))
-== fold ((\ ia -> let n = ia * card @b in
-             fold ((\ ib -> h (n + ib)) . fromEnum @b) . fromEnum @a)
-== fold (unTarr (TArr @a (\ ia -> let n = ia * card @b in
-                            fold (unTarr (TArr @b (\ ib -> h (n + ib)))))))
-== fold (TArr @a (\ ia -> let n = ia * card @b in
-                            fold (TArr @b (\ ib -> h (n + ib)))))
-
-#endif
+lsums :: (Traversable f, Num a) => f a -> f a :* a
+lsums = (fmap getSum *** getSum) . lscan . fmap Sum
