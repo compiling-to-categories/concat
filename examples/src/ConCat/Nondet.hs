5d40d5595667ca79da3f2555c31d20315bf56004
Conal Elliott <conal@conal.net>
2017-08-22 12:46:34 -0700

    Ok k p

--- examples/src/ConCat/Nondet.hs
+++ examples/src/ConCat/Nondet.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE FlexibleInstances #-}
@@ -5,6 +8,8 @@
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
@@ -18,44 +23,48 @@ import GHC.Types (Constraint)
 
 import ConCat.Category
 
-type C p = (() :: Constraint)
-
-data ND k a b = forall p. C p => ND (p -> (a `k` b))  -- for some constraint C
-
-exactly :: (a `k` b) -> ND k a b
+data ND k a b = forall p. Ok k p => ND (p -> (a `k` b))
+  
+exactly :: OkUnit k => (a `k` b) -> ND k a b
 exactly f = ND (\ () -> f)
 
-instance Category k => Category (ND k) where
+instance (Category k, OkProd k, OkUnit k) => Category (ND k) where
   type Ok (ND k) = Ok k
   id = exactly id
-  ND g . ND f = ND (\ (p,q) -> g q . f p)
+  -- ND g . ND f = ND (\ (p,q) -> g q . f p)
+  ND (g :: q -> (b `k` c)) . ND (f :: p -> (a `k` b)) = ND (\ (p,q) -> g q . f p) <+ okProd @k @p @q
 
-instance ProductCat k => ProductCat (ND k) where
+--   The constraint ‘OkProd k’ is no smaller than the instance head
+--   (Use UndecidableInstances to permit this)
+
+instance (ProductCat k, OkUnit k) => ProductCat (ND k) where
   exl = exactly exl
   exr = exactly exr 
-  ND f &&& ND g = ND (\ (p,q) -> f p &&& g q)
+  -- ND f &&& ND g = ND (\ (p,q) -> f p &&& g q)
+  ND (f :: p -> (a `k` c)) &&& ND (g :: q -> (a `k` d)) = ND (\ (p,q) -> f p &&& g q) <+ okProd @k @p @q
 
-instance CoproductCat k => CoproductCat (ND k) where
+instance (CoproductCat k, OkProd k, OkUnit k) => CoproductCat (ND k) where
   inl = exactly inl
   inr = exactly inr 
-  ND f ||| ND g = ND (\ (p,q) -> f p ||| g q)
+  -- ND f ||| ND g = ND (\ (p,q) -> f p ||| g q)
+  ND (f :: p -> (a `k` c)) ||| ND (g :: q -> (b `k` c)) = ND (\ (p,q) -> f p ||| g q) <+ okProd @k @p @q
 
-instance DistribCat k => DistribCat (ND k) where
+instance (DistribCat k, OkUnit k) => DistribCat (ND k) where
   distl = exactly distl
   distr = exactly distr 
 
-instance ClosedCat k => ClosedCat (ND k) where
+instance (ClosedCat k, OkUnit k) => ClosedCat (ND k) where
   apply = exactly apply
   curry (ND f) = ND (curry . f)
   uncurry (ND g) = ND (uncurry . g)
 
-instance TerminalCat k => TerminalCat (ND k) where
+instance (TerminalCat k, OkProd k, OkUnit k) => TerminalCat (ND k) where
   it = exactly it
 
-instance ConstCat k b => ConstCat (ND k) b where
+instance (ConstCat k b, OkProd k, OkUnit k) => ConstCat (ND k) b where
   const b = exactly (const b)
 
-instance (NumCat k a, ProductCat k) => NumCat (ND k) a where
+instance (NumCat k a, ProductCat k, OkUnit k) => NumCat (ND k) a where
   addC    = exactly addC
   mulC    = exactly mulC
   negateC = exactly negateC
