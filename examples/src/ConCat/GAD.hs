a81abac1975bfeffe7f210d65245747e02163644
Conal Elliott <conal@conal.net>
2017-09-12 12:23:48 -0700

    parens for clarity

--- examples/src/ConCat/GAD.hs
+++ examples/src/ConCat/GAD.hs
@@ -34,7 +34,7 @@ import ConCat.AltCat
 import ConCat.Rep
 
 -- newtype GD k a b = D { unD :: a -> b :* (a `k` b) }
-data GD k a b = D { unD :: a -> b :* (a `k` b) }
+data GD k a b = D { unD :: a -> (b :* (a `k` b)) }
 
 -- Differentiable linear function, given the function and linear map version
 linearD :: (a -> b) -> (a `k` b) -> GD k a b
