798ddc7347382874d8d37e0e46b553ea5065f5fa
Conal Elliott <conal@conal.net>
2017-09-16 12:11:21 -0700

    left and right finite types

--- examples/src/ConCat/Shaped.hs
+++ examples/src/ConCat/Shaped.hs
@@ -14,33 +14,35 @@
 
 module ConCat.Shaped (module ConCat.Shaped, module ConCat.Pair) where
 
+import Data.Void (Void)
 import GHC.Generics hiding (S)
 
+import ConCat.Misc ((:+))
 import ConCat.Nat
 import ConCat.Pair
-
--- | Right-associated vector
-type family RVec n where
-  RVec Z     = U1
-  RVec (S n) = Par1 :*: RVec n
 
 -- | Left-associated vector
 type family LVec n where
   LVec Z     = U1
   LVec (S n) = LVec n :*: Par1
 
--- | Right-associated functor exponentiation
-type family RPow h n where
-  RPow h Z     = Par1
-  RPow h (S n) = h :.: RPow h n
+-- | Right-associated vector
+type family RVec n where
+  RVec Z     = U1
+  RVec (S n) = Par1 :*: RVec n
 
 -- | Left-associated functor exponentiation
 type family LPow h n where
   LPow h Z     = Par1
   LPow h (S n) = LPow h n :.: h
 
-type RBin n = RPow Pair n
+-- | Right-associated functor exponentiation
+type family RPow h n where
+  RPow h Z     = Par1
+  RPow h (S n) = h :.: RPow h n
+
 type LBin n = LPow Pair n
+type RBin n = RPow Pair n
 
 -- Bushy binary leaf trees
 type family Bush n where
@@ -59,3 +61,28 @@ type family Bush' n where
 -- "data Bush a = NilB | ConsB a (Bush (Bush a))", adding depth-indexing. It
 -- works fine for maps, folds, traversals, and scans. The use of (:*:) thwarts
 -- FFT.
+
+-- | @[0..n-1]@, left version
+type family LFin n where
+  LFin Z = Void
+  LFin (S n) = LFin n :+ ()
+
+-- | @[0..n-1]@, right version
+type family RFin n where
+  RFin Z = Void
+  RFin (S n) = () :+ RFin n
+
+#if 0
+
+-- Functor versions
+
+-- | @[0..n-1]@, left version
+type family LFin' n where
+  LFin' Z = V1
+  LFin' (S n) = LFin' n :+: U1
+
+-- | @[0..n-1]@, right version
+type family RFin' n where
+  RFin' Z = V1
+  RFin' (S n) = U1 :+: RFin' n
+#endif
