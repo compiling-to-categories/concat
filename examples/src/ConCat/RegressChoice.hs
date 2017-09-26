55d7395748ba1de5314a6e163f53fccba920b0b0
Conal Elliott <conal@conal.net>
2017-09-26 08:40:49 -0700

    tidy

--- examples/src/ConCat/RegressChoice.hs
+++ examples/src/ConCat/RegressChoice.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE AllowAmbiguousTypes #-}
 
@@ -11,16 +14,18 @@
 
 module ConCat.RegressChoice where
 
+import GHC.Types (Constraint)
+
 import Data.Key
 
-import ConCat.Misc (R,sqr,Unop,(:*),Yes1)
+import ConCat.Misc (R,sqr,Unop,Binop,(:*),Yes1)
 import ConCat.Free.VectorSpace
 -- import ConCat.Free.LinearRow
 -- import ConCat.Shaped
 
-import ConCat.AD (gradient)
+import ConCat.AD (gradient,IsScalar)
 
-import ConCat.Choice (choose)
+import ConCat.Choice
 
 -- First do a simple linear regression
 
@@ -29,23 +34,34 @@ type Line = (R,R)
 
 type Fun = R -> R
 
--- -- | 1D linear function in slope/intercept for: y = m * x + b.
--- -- Defines a family of functions.
--- line :: Line -> Fun
--- line (m,b) x = m * x + b
+-- | 1D linear function in slope/intercept for: y = m * x + b.
+-- Defines a family of functions.
+line :: Line -> Fun
+line (m,b) x = m * x + b
+{-# INLINE line #-}
 
--- Choose a 1D linear function. To be re-interpreted in a ChoiceCat.
-chooseLine :: Fun
-chooseLine = choose @(HasV R) (\ (m,b) x -> m * x + b)
+-- -- Choose a 1D linear function. To be re-interpreted in a ChoiceCat.
+-- chooseLine :: Fun
+-- chooseLine = choose @(HasV R) line
+-- -- chooseLine = choose @(HasV R) (\ (m,b) x -> m * x + b)
+-- {-# INLINE chooseLine #-}
 
--- chooseLine' :: forall con. Fun
--- chooseLine' = choose @con (\ (m,b) x -> m * x + b)
+-- chooseLine :: forall con. (con Line, CartCon con) => Fun
+-- -- chooseLine = choose @con (\ (m,b) x -> m * x + b)
+-- chooseLine = choose @con line
+-- {-# INLINE chooseLine #-}
 
 type Sample = (R,R)
 
 -- | Squared error for a single sample
-sqErr :: Sample -> Fun -> R
-sqErr (a,b) f = sqr (f a - b)
+sqErr :: (HasV s b, f ~ V s b, Zip f, Foldable f, Num s) => (a,b) -> (a -> b) -> s
+-- sqErr (a,b) f = distSqr (toV (f a)) (toV b)
+sqErr (a,b) f = distSqr' (f a) b
+{-# INLINE sqErr #-}
+
+-- sqErr :: Sample -> Fun -> R
+-- -- sqErr :: (Num b) => (a,b) -> (a -> b) -> b
+-- sqErr (a,b) f = sqr (f a - b)
 
 -- lineErr :: Sample -> Line -> R
 -- lineErr s = sqErr s . line
@@ -55,7 +71,18 @@ sqErr (a,b) f = sqr (f a - b)
 -- step s = negateV' @R . gradient (lineErr s)
 -- -- step s l = negateV' @R (gradient (lineErr s) l)
 
--- bestChoice :: Choice (->) 
+step :: forall s p a b. ( Num s, IsScalar s, HasV s b, Zip (V s b), Foldable (V s b)
+                        , HasV s p, Functor (V s p))
+     => (p -> a -> b) -> (a,b) -> (p -> p)
+step f ab = negateV' @s . gradient @s (\ p -> sqErr ab (f p))
+{-# INLINE step #-}
+
+--                                             f     :: p -> a -> b
+--                        (\ p ->              f p)  :: p -> a -> b
+--                        (\ p -> sqErr (a,b) (f p)) :: p -> s
+--               gradient (\ p -> sqErr (a,b) (f p)) :: p -> p
+-- negateV' @s . gradient (\ p -> sqErr (a,b) (f p)) :: p -> p
+
 
 {--------------------------------------------------------------------
     Miscellany
@@ -63,3 +90,9 @@ sqErr (a,b) f = sqr (f a - b)
 
 negateV' :: forall s a. (HasV s a, Functor (V s a), Num s) => Unop a
 negateV' = onV @s negateV
+
+subV' :: forall s a. (HasV s a, Zip (V s a), Num s) => Binop a
+subV' = onV2 @s subV
+
+distSqr' :: (HasV s a, f ~ V s a, Zip f, Foldable f, Num s) => a -> a -> s
+distSqr' a a' = distSqr (toV a) (toV a')
