8823c7b776243a1dab582f85e7fbbee0f79f0a45
Conal Elliott <conal@conal.net>
2017-10-20 08:43:06 -0700

    (==), (/=)

--- inline/src/ConCat/Inline/SampleMethods.hs
+++ inline/src/ConCat/Inline/SampleMethods.hs
@@ -1,21 +1,22 @@
-
 {-# OPTIONS_GHC -Wall #-}
-{-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
+-- {-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
 
--- | 
+-- | Class-op aliases that have been inlined to dictionary method accessors.
+-- To see the effect,
+--   ghc --show-iface SampleMethods.hi
 
--- ghc --show-iface SampleMethods.hi
-
-{-# OPTIONS_GHC -ddump-simpl #-}
+-- {-# OPTIONS_GHC -ddump-simpl #-}
 -- {-# OPTIONS_GHC -dverbose-core2core #-}
 -- {-# OPTIONS_GHC -ddump-rule-rewrites #-}
 -- {-# OPTIONS_GHC -ddump-rules #-}
 
--- {-# OPTIONS_GHC -fplugin=ConCat.Inline.Plugin #-}
+{-# OPTIONS_GHC -fplugin=ConCat.Inline.Plugin #-}
 
 module ConCat.Inline.SampleMethods where
 
+import qualified Prelude as P
 import ConCat.Inline.ClassOp (inline)
 
-eq' :: Eq a => a -> a -> Bool
-eq' = inline (==)
+(==), (/=) :: P.Eq a => a -> a -> P.Bool
+(==) = inline (P.==)
+(/=) = inline (P./=)
