79f8513a336790d7b22f6b78a01bd7bf62521b18
Conal Elliott <conal@conal.net>
2017-11-18 14:06:18 -0800

    tidied

--- examples/src/ConCat/Regress.hs
+++ examples/src/ConCat/Regress.hs
@@ -18,7 +18,7 @@ module ConCat.Regress where
 
 import Data.Key (Zip(..))
 
-import ConCat.Misc ((:*),Binop)
+import ConCat.Misc ((:*)) -- ,Binop
 import ConCat.AltCat (Ok)
 import qualified ConCat.Free.VectorSpace as V
 import ConCat.Free.VectorSpace hiding ((^+^),distSqr)
@@ -28,15 +28,22 @@ import ConCat.Free.LinearRow
 -- There's not much here now. See ConCat.Free.Affine.
 
 -- Square of distance between predicted and observed datum
-sqErr :: Ok (L s) b => a :* b -> (a -> b) -> s
+sqErrF :: forall s a g. (Num s, Foldable g, Zip g)
+       => a :* g s -> (a -> g s) -> s
+sqErrF (x,y) h = h x `V.distSqr` y
+{-# INLINE sqErrF #-}
+
+-- Square of distance between predicted and observed datum
+sqErr :: forall s a b. Ok (L s) b => a :* b -> (a -> b) -> s
 sqErr (x,y) h = h x `distSqr` y
 {-# INLINE sqErr #-}
 
-add :: forall s a. (HasV s a, Zip (V s a), Num s) => Binop a
-add = onV2 @s (V.^+^)
-{-# INLINE add #-}
+-- add :: forall s a. (HasV s a, Zip (V s a), Num s) => Binop a
+-- add = onV2 @s (V.^+^)
+-- {-# INLINE add #-}
 
 -- | Distance squared
-distSqr :: forall s a. (HasV s a, Zip (V s a), Foldable (V s a), Num s) => a -> a -> s
+distSqr :: forall s a. (HasV s a, Zip (V s a), Foldable (V s a), Num s)
+        => a -> a -> s
 distSqr a b = V.distSqr (toV a) (toV b)
 {-# INLINE distSqr #-}
