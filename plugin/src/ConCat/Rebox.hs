05288ccaaaddb2734f9833c3343f9150fd68dd67
Conal Elliott <conal@conal.net>
2017-11-23 10:13:59 -0800

    more comment elaboration

--- plugin/src/ConCat/Rebox.hs
+++ plugin/src/ConCat/Rebox.hs
@@ -118,9 +118,11 @@ Rebox2I((*#),(*))
 Rebox1(boxD,unboxI,double2Int#,truncateC)
 
 -- Generating truncateC instead of truncate to avoid an infinite rewrite loop
--- between this rule and GHC's "truncat/Double->Int" rule. Maybe change all of
+-- between this rule and GHC's "truncate/Double->Int" rule. Maybe change all of
 -- the generated functions to be the categorical versions to more robustly avoid
--- such loops. This change would make rewriting a little more efficient as well.
+-- such loops. This change would make rewriting a little more efficient as well,
+-- since operations like truncate would get rewritten to their counterparts like
+-- truncateC anyway.
 
 Rebox1F(negateFloat#,negate)
 Rebox2F(plusFloat#,(+))
