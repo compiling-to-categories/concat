8ee7d7aa86d57f5b25952d806616af69b1afd2ac
Sandy Maguire <sandy@sandymaguire.me>
2017-12-12 17:05:41 +0700

    tests that mostly fail

--- plugin/test/SimpleSpec.hs
+++ plugin/test/SimpleSpec.hs
@@ -1,6 +1,7 @@
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# OPTIONS_GHC -fdicts-strict          #-}
 {-# OPTIONS_GHC -fexpose-all-unfoldings #-}
-{-# OPTIONS_GHC -fdicts-strict #-}
 
 module SimpleSpec where
 
@@ -9,15 +10,67 @@ import Test.Hspec
 import Types
 
 
-
 spec :: Spec
-spec = describe "test" $ do
-  it "should compile" $
-    CFloat 5 `shouldNotBe` toCcc (id @Float)
+spec = describe "free syntactic ccc" $ do
+  it "id" $ toCcc id `shouldBe` CId
 
-  it "should compile" $
-    CFloat 5 `shouldNotBe` toCcc (double @Float)
+  it "const float" $
+    toCcc (const @Float 5)
+      `shouldBe`
+        CConst 5
 
-double :: Num a => a -> a
-double = \x -> x + x
+  it "const inequal bools" $
+    toCcc (const True)
+      `shouldNotBe`
+        CConst False
+
+  -- fails: "(const False)"
+  -- NOTE(sandy): this is caused by GHC simplifying
+  it "compose consts (not simplified)" $
+    toCcc (\x -> const False (const True x))
+      `shouldBe`
+        CComp (CConst False) (CConst True)
+
+  it "compose consts (simplified)" $
+    toCcc (\x -> const False (const True x))
+      `shouldBe`
+        CConst False
+
+  -- fails: "(const ())"
+  --   nb: this "()" is haskell's unit, not 'it'
+  -- NOTE(sandy): the plugin does not seem to ever emit 'it'
+  it "const unit" $
+    toCcc (const ())
+      `shouldBe`
+        CTerm
+
+  -- fails: "(comp app (and (curry exr) exl)))"
+  it "exl" $
+    toCcc fst
+      `shouldBe`
+        CExl
+
+  -- fails: "(comp app (and (curry exr) exr)))"
+  it "exr" $
+    toCcc (\(_, b) -> b)
+      `shouldBe`
+        CExr
+
+  -- fails: "Oops: toCcc' called"
+  it "inl" $
+    toCcc Left
+      `shouldBe`
+        CInl
+
+  -- fails: "Oops: toCcc' called"
+  it "inr" $
+    toCcc Right
+      `shouldBe`
+        CInr
+
+  -- -- fails to compile: "panic! lam Case of boxer: bare unboxed var"
+  -- it "twice" $
+  --   toCcc (double @Float)
+  --     `shouldBe`
+  --       CComp CAdd (CId `CPAnd` CId)
 
--- plugin/test/Types.hs
+++ plugin/test/Types.hs
@@ -1,48 +1,85 @@
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE GADTs                 #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE StandaloneDeriving    #-}
 
 module Types where
 
 import qualified ConCat.Category as CC
-import           Test.Hspec
-import Unsafe.Coerce (unsafeCoerce)
+import           Data.Typeable
+import           Unsafe.Coerce (unsafeCoerce)
 
 
-data Chillin a b where
-  CId   :: Chillin a b
-  CComp :: Chillin b c -> Chillin a b -> Chillin a c
-  CFloat :: Float -> Chillin a Float
-  CExl :: Chillin (a, b) a
-  CExr :: Chillin (a, b) b
-  CPAnd :: Chillin a b -> Chillin a c -> Chillin a (b, c)
-  CCurry :: Chillin (a, b) c -> Chillin a (b -> c)
-  CApply :: Chillin (a -> b, a) b
-  CMul :: Chillin (a, a) a
-  CAdd :: Chillin (a, a) a
-  CPow :: Chillin (a, Int) a
-  CNeg :: Chillin a a
+------------------------------------------------------------------------------ | The free category construction over all of the relevant concat classes.
+data FreeSyn a b where
+  CId    :: FreeSyn a b
+  CComp  :: FreeSyn b c -> FreeSyn a b -> FreeSyn a c
+  CTerm  :: FreeSyn a ()
+  CExl   :: FreeSyn (a, b) a
+  CExr   :: FreeSyn (a, b) b
+  CPAnd  :: FreeSyn a b -> FreeSyn a c -> FreeSyn a (b, c)
+  CCurry :: FreeSyn (a, b) c -> FreeSyn a (b -> c)
+  CApply :: FreeSyn (a -> b, a) b
+  CMul   :: FreeSyn (a, a) a
+  CAdd   :: FreeSyn (a, a) a
+  CPow   :: FreeSyn (a, Int) a
+  CNeg   :: FreeSyn a a
+  CInl   :: FreeSyn a (Either a b)
+  CInr   :: FreeSyn b (Either a b)
+  CCOr   :: FreeSyn a c -> FreeSyn b c -> FreeSyn (Either a b) c
+  CConst :: (Show b, Eq b, Typeable b) => b -> FreeSyn a b
 
-deriving instance Show (Chillin a b)
 
-instance Eq (Chillin a b) where
+instance Show (FreeSyn a b) where
+  show CId         = "id"
+  show (CComp a b) = "(comp " ++ show a ++ " " ++ show b ++ ")"
+  show CTerm       = "unit"
+  show CExl        = "exl"
+  show CExr        = "exr"
+  show (CPAnd a b) = "(and " ++ show a ++ " " ++ show b ++ ")"
+  show (CCurry a)  = "(curry " ++ show a ++ ")"
+  show CApply      = "app"
+  show CMul        = "mul"
+  show CAdd        = "add"
+  show CPow        = "pow"
+  show CNeg        = "neg"
+  show CInl        = "inl"
+  show CInr        = "inr"
+  show (CCOr a b)  = "(or " ++ show a ++ " " ++ show b ++ ")"
+  show (CConst a)  = "(const " ++ show a ++ ")"
+
+
+instance Eq (FreeSyn a b) where
   CId == CId = True
   CComp a b == CComp c d =
+    -- safe enough for test code, unless we do a bad coerce of 'CConst' in
+    -- which case bad things will happen
     and [ unsafeCoerce a == c
         , unsafeCoerce b == d
         ]
-  CFloat a == CFloat b = a == b
-  _ == _ = False
-  {-# INLINE (==) #-}
+  CTerm     == CTerm = True
+  CExl      == CExl = True
+  CExr      == CExr = True
+  CPAnd a b == CPAnd c d = a == c && b == d
+  CCurry a  == CCurry b = a == b
+  CMul      == CMul = True
+  CAdd      == CAdd = True
+  CPow      == CPow = True
+  CNeg      == CNeg = True
+  CInl      == CInl = True
+  CInr      == CInr = True
+  CCOr a b  == CCOr c d = a == c && b == d
+  CConst a  == CConst b = cast a == Just b
+  _         == _ = False
 
-instance CC.Category Chillin where
+
+instance CC.Category FreeSyn where
   id = CId
   (.) = CComp
   {-# INLINE id #-}
   {-# INLINE (.) #-}
 
-instance CC.ProductCat Chillin where
+
+instance CC.ProductCat FreeSyn where
   exl = CExl
   exr = CExr
   (&&&) = CPAnd
@@ -50,13 +87,15 @@ instance CC.ProductCat Chillin where
   {-# INLINE exr #-}
   {-# INLINE (&&&) #-}
 
-instance CC.ClosedCat Chillin where
+
+instance CC.ClosedCat FreeSyn where
   curry = CCurry
   apply = CApply
   {-# INLINE curry #-}
   {-# INLINE apply #-}
 
-instance Num a => CC.NumCat Chillin a where
+
+instance Num a => CC.NumCat FreeSyn a where
   addC = CAdd
   mulC = CMul
   negateC = CNeg
@@ -65,4 +104,23 @@ instance Num a => CC.NumCat Chillin a where
   {-# INLINE mulC #-}
   {-# INLINE negateC #-}
   {-# INLINE powIC #-}
+
+
+instance CC.CoproductCat FreeSyn where
+  inl = CInl
+  inr = CInr
+  (|||) = CCOr
+  {-# INLINE inl #-}
+  {-# INLINE inr #-}
+  {-# INLINE (|||) #-}
+
+
+instance CC.TerminalCat FreeSyn where
+  it = CTerm
+  {-# INLINE it #-}
+
+
+instance (Show a, Eq a, Typeable a) => CC.ConstCat FreeSyn a where
+  const = CConst
+  {-# INLINE const #-}
 
