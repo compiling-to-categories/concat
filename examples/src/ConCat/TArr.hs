2d3e250789cdf34085d412af578a4cf5f1fba9a3
Conal Elliott <conal@conal.net>
2018-01-31 15:55:48 -0800

    Domain-typed arrays

---
+++ examples/src/ConCat/TArr.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
+
+-- | Domain-typed arrays
+
+module ConCat.TArr where
+
+import GHC.TypeLits (KnownNat)
+import GHC.Types (Nat)
+
+import Data.Finite
+import Data.Vector.Sized
+
+import ConCat.Misc ((:*),(:+))
+
+class KnownNat (Card a) => HasFin a where
+  type Card a :: Nat
+  toFin :: a -> Finite (Card a)
+  unFin :: Finite (Card a) -> a
+
+-- instance HasFin ()         where -- ...
+-- instance HasFin Bool       where -- ...
+-- instance HasFin (Finite n) where -- ...
+
+-- instance (HasFin a, HasFin b) => HasFin (a :+ b) where -- ...
+-- instance (HasFin a, HasFin b) => HasFin (a :* b) where -- ...
+
+-- Domain-typed "arrays"
+newtype Arr a b = Arr (Vector (Card a) b)
+
+(!) :: HasFin a => Arr a b -> (a -> b)
+Arr v ! a = v `index` toFin a
+
+arrTrie :: (HasFin a, HasTrie a) => Arr a b -> Trie a b
+arrTrie = toTrie . (!)
+
+-- where
+class HasTrie a where
+  type Trie a :: * -> *
+  toTrie :: (a -> b) -> Trie a b
+  unTrie :: Trie a b -> (a -> b)
