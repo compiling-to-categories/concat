51f395c5998d1db7b72288418fd43c54af68d774
David Banas <capn.freako@gmail.com>
2018-02-01 17:10:04 -0800

    Initial HasFin instance definitions.

--- examples/src/ConCat/TArr.hs
+++ examples/src/ConCat/TArr.hs
@@ -1,8 +1,11 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
@@ -11,9 +14,11 @@
 
 module ConCat.TArr where
 
-import GHC.TypeLits (KnownNat)
+-- import GHC.TypeLits (KnownNat)
+import GHC.TypeLits
 import GHC.Types (Nat)
 
+import Data.Proxy
 import Data.Finite
 import Data.Vector.Sized
 
@@ -24,12 +29,44 @@ class KnownNat (Card a) => HasFin a where
   toFin :: a -> Finite (Card a)
   unFin :: Finite (Card a) -> a
 
--- instance HasFin ()         where -- ...
--- instance HasFin Bool       where -- ...
--- instance HasFin (Finite n) where -- ...
+instance HasFin () where
+  type Card () = 1
+  toFin _ = finite 0
+  unFin _ = ()
 
--- instance (HasFin a, HasFin b) => HasFin (a :+ b) where -- ...
--- instance (HasFin a, HasFin b) => HasFin (a :* b) where -- ...
+instance HasFin Bool where
+  type Card Bool = 2
+
+  toFin False = finite 0
+  toFin True  = finite 1
+
+  unFin x = case (getFinite x) of
+              0 -> False
+              1 -> True
+              _ -> error "Yikes! We just got a numerical representation of Bool >1."
+
+instance KnownNat n => HasFin (Finite n) where
+  type Card (Finite n) = n
+  toFin = id
+  unFin = id
+
+instance (HasFin a, HasFin b, KnownNat (Card a + Card b)) => HasFin (a :+ b) where
+  type Card (a :+ b) = Card a + Card b
+
+  toFin (Left  x) = (finite . getFinite . toFin) x
+  toFin (Right y) = finite $ natVal (Proxy @(Card a)) + (natVal . toFin) y
+
+  unFin n = if getFinite n < natVal (Proxy @(Card a))
+               then Left  ((unFin . finite . getFinite) n)
+               else Right ((unFin . finite) $ getFinite n - natVal (Proxy @(Card a)))
+
+instance (HasFin a, HasFin b, KnownNat (Card a * Card b)) => HasFin (a :* b) where
+  type Card (a :* b) = Card a * Card b
+
+  toFin (x,y) = finite $ (getFinite . toFin) x * (natVal (Proxy @(Card b))) + (getFinite . toFin) y
+
+  unFin n = let (q,r) = (getFinite . toFin) n `divMod` (natVal (Proxy @(Card b)))
+             in ((unFin . finite) q, (unFin . finite) r)
 
 -- Domain-typed "arrays"
 newtype Arr a b = Arr (Vector (Card a) b)
