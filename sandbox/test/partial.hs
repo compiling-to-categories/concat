dbcdf660674884ebfb2c7aa8dd0b5d6db6acbff8
David Banas <capn.freako@gmail.com>
2018-02-06 09:59:52 -0800

    First attempt at concat-sandbox:partial

---
+++ sandbox/test/partial.hs
@@ -0,0 +1,20 @@
+-- Try to "see" partial function implementation in Core.
+
+{-# LANGUAGE DataKinds #-}
+
+{-# OPTIONS_GHC -Wall #-}
+-- {-# OPTIONS_GHC -dsimpl #-}
+
+module Main where
+
+-- import GHC.TypeLits
+import Data.Finite
+
+main :: IO ()
+main = do
+  putStrLn $ "\ntoFin False = " ++ show (toFin False)
+
+toFin :: Bool -> Finite 2
+toFin False = finite 0
+toFin True  = finite 1
+
