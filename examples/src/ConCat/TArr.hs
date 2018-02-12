85cbc4aefe3b1ad930cac42bd0ddb7c4b3046f89
David Banas <capn.freako@gmail.com>
2018-02-12 12:46:52 -0800

    Completed rewrite of HasFin instances for products and sums, using isomorphisms.

--- examples/src/ConCat/TArr.hs
+++ examples/src/ConCat/TArr.hs
@@ -23,7 +23,7 @@ import GHC.Types (Nat)
 import Data.Proxy
 import Data.Finite
 import Data.Finite.Internal (Finite(..))
-import Data.Vector.Sized
+-- import Data.Vector.Sized
 import qualified Data.Vector.Sized as V
 
 import ConCat.Misc ((:*),(:+))
@@ -38,14 +38,44 @@ data a <-> b = Iso (a -> b) (b -> a)
 
 type KnownNat2 m n = (KnownNat m, KnownNat n)
 
-finSum  :: KnownNat2 m n => Finite (m + n) <-> (Finite m :+ Finite n)
-finSum  = undefined
+finSum :: KnownNat2 m n => Finite (m + n) <-> (Finite m :+ Finite n)
+finSum = Iso g h
+  where g :: forall m n. KnownNat2 m n => Finite (m + n) -> Finite m :+ Finite n
+        g (Finite l) = if l >= (natValAt @m)
+                          then Right $ Finite (l - (natValAt @m))
+                          else Left  $ Finite l
 
-finProd :: KnownNat2 m n => Finite (m * n) <-> Finite m :* Finite n
-finProd = undefined
+        h :: forall m n. KnownNat2 m n => Finite m :+ Finite n -> Finite (m + n)
+        h (Left  (Finite l)) = Finite l  -- Need to do it this way, for type conversion.
+        h (Right (Finite k)) = Finite (k + (natValAt @m))
 
-finExp  :: KnownNat2 m n => Finite (m ^ n) <-> Finite m :^ Finite n
-finExp  = undefined
+finProd :: KnownNat2 m n => Finite (m * n) <-> (Finite m :* Finite n)
+finProd = Iso g h
+  where g :: forall m n. KnownNat2 m n => Finite (m * n) -> Finite m :* Finite n
+        g (Finite l) = let (q,r) = l `divMod` (natValAt @n)
+                        in (Finite q, Finite r)
+
+        h :: forall m n. KnownNat2 m n => Finite m :* Finite n -> Finite (m * n)
+        h (Finite l, Finite k) = Finite $ l * (natValAt @n) + k
+
+finExp :: KnownNat2 m n => Finite (m ^ n) <-> (Finite m :^ Finite n)
+finExp = undefined
+-- finExp = Iso g h
+--   where g :: forall m n. KnownNat2 m n => Finite (m ^ n) -> Finite m :^ Finite n
+--         -- g (Finite l) =  -- Unique, n-element factorization? What about ordering?
+--         g = undefined
+
+--         h :: forall m n. KnownNat2 m n => Finite m :^ Finite n -> Finite (m ^ n)
+--         h f = Finite $ product [(inFin f) l | l <- [0..((natValAt @n)-1)]]
+
+--         inFin :: (Finite n -> Finite m) -> Integer -> Integer
+--         inFin f' = unFin . f' . toFin
+
+isoFwd :: a <-> b -> a -> b
+isoFwd (Iso g _) = g
+
+isoRev :: a <-> b -> b -> a
+isoRev (Iso _ h) = h
 
 {----------------------------------------------------------------------
    A class of types with known finite representations.
@@ -78,32 +108,44 @@ instance KnownNat n => HasFin (Finite n) where
 instance (HasFin a, HasFin b, KnownNat (Card a + Card b)) => HasFin (a :+ b) where
   type Card (a :+ b) = Card a + Card b
 
-  toFin = let (Iso _ h) = finSum
-           in \case
-                (Left  x) -> h $ Left  (toFin x)
-                (Right y) -> h $ Right (toFin y)
+  -- I really want to write this as:
+  --   toFin = h . fmap toFin
+  -- but I can't, due to the fact that sums are only functorial in their second type argument.
+  -- We can get around this limitation, by wrapping (:+), but only when a ~ b.
+  toFin = \case
+            (Left  x) -> h $ Left  (toFin x)
+            (Right y) -> h $ Right (toFin y)
+    where h = isoRev finSum
 
-  unFin n = let (Iso g _) = finSum
-             in case (g n) of
-                (Left  m) -> Left  (unFin m)
-                (Right l) -> Right (unFin l)
+  unFin n = case (g n) of
+              (Left  m) -> Left  (unFin m)
+              (Right l) -> Right (unFin l)
+    where g = isoFwd finSum
 
 instance (HasFin a, HasFin b, KnownNat (Card a * Card b)) => HasFin (a :* b) where
   type Card (a :* b) = Card a * Card b
 
-  toFin (x,y) = finite $ (getFinite . toFin) x * (natVal (Proxy @(Card b))) + (getFinite . toFin) y
+  toFin (x,y) = isoRev finProd (toFin x, toFin y)
 
-  unFin n = let (q,r) = (getFinite . toFin) n `divMod` (natVal (Proxy @(Card b)))
-             in ((unFin . finite) q, (unFin . finite) r)
+  unFin n = let (m, l) = isoFwd finProd n
+             in (unFin m, unFin l)
+
+instance (HasFin a, HasFin b, KnownNat (Card a ^ Card b)) => HasFin (a :^ b) where
+  type Card (a :^ b) = Card a ^ Card b
+
+  toFin f = isoRev finExp (toFin . f . unFin)
+
+  unFin n = let f' = isoFwd finExp n
+             in (unFin . f' . toFin)
 
 -- Domain-typed "arrays"
-newtype Arr a b = Arr (Vector (Card a) b)
+newtype Arr a b = Arr (V.Vector (Card a) b)
 
 instance Functor (Arr a) where
   fmap f (Arr v) = Arr $ V.map f v
 
 (!) :: HasFin a => Arr a b -> (a -> b)
-Arr v ! a = v `index` toFin a
+Arr v ! a = v `V.index` toFin a
 
 arrTrie :: (HasFin a, HasTrie a) => Arr a b -> Trie a b
 arrTrie = toTrie . (!)
