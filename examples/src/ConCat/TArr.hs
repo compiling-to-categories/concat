56f385bf55f9319a81dcbe070caa1b9950f02a47
David Banas <capn.freako@gmail.com>
2018-02-13 15:48:14 -0800

    Some additional refinements to TArr.hs.

--- examples/src/ConCat/TArr.hs
+++ examples/src/ConCat/TArr.hs
@@ -18,22 +18,21 @@
 
 module ConCat.TArr where
 
-import Prelude hiding (id, (.))  -- Coming from Control.Category.
+import Prelude hiding (id, (.), const)  -- Coming from Control.Category.
 
 import GHC.TypeLits
 import GHC.Types (Nat)
 
-import Control.Arrow        (first, second)
+import Control.Arrow         (first, second)
 import qualified Control.Arrow as A
-import Control.Category
 import Data.Proxy
 import Data.Finite
-import Data.Finite.Internal (Finite(..))
+import Data.Finite.Internal  (Finite(..))
 import qualified Data.Vector.Sized as V
 
--- import ConCat.AltCat
--- import ConCat.Category ((+++),(***))
-import ConCat.Misc     ((:*), (:+), cond)
+import ConCat.AltCat
+import qualified ConCat.Category
+import ConCat.Misc           ((:*), (:+), cond)
 
 {----------------------------------------------------------------------
    Some useful isomorphisms.
@@ -45,16 +44,13 @@ data a <-> b = Iso (a -> b) (b -> a)
 
 instance Category (<->) where
   id = Iso id id
-  (Iso g h) . (Iso g' h') = Iso (g . g') (h' . h)
+  Iso g h . Iso g' h' = Iso (g . g') (h' . h)
 
-infixr 3 ***
-infixr 2 +++
+instance MonoidalPCat (<->) where
+  Iso g h *** Iso g' h' = Iso (g A.*** g') (h A.*** h')
 
-(***) :: (a <-> c) -> (b <-> d) -> ((a :* b) <-> (c :* d))
-Iso g h *** Iso g' h' = Iso (g A.*** g') (h A.*** h')
-
-(+++) :: (a <-> c) -> (b <-> d) -> ((a :+ b) <-> (c :+ d))
-Iso g h +++ Iso g' h' = Iso (g A.+++ g') (h A.+++ h')
+instance MonoidalSCat (<->) where
+  Iso g h +++ Iso g' h' = Iso (g A.+++ g') (h A.+++ h')
 
 type KnownNat2 m n = (KnownNat m, KnownNat n)
 
@@ -78,24 +74,17 @@ finProd = Iso h g
         h :: forall m n. KnownNat2 m n => Finite m :* Finite n -> Finite (m * n)
         h (Finite l, Finite k) = Finite $ l * natValAt @n + k
 
--- finExp :: KnownNat2 m n => Finite (m ^ n) <-> (Finite m :^ Finite n)
--- finExp = undefined
--- finExp = Iso g h
---   where g :: forall m n. KnownNat2 m n => Finite (m ^ n) -> Finite m :^ Finite n
---         -- g (Finite l) =  -- Unique, n-element factorization? What about ordering?
---         g = undefined
-
---         h :: forall m n. KnownNat2 m n => Finite m :^ Finite n -> Finite (m ^ n)
---         h f = Finite $ product [(inFin f) l | l <- [0..((natValAt @n)-1)]]
-
---         inFin :: (Finite n -> Finite m) -> Integer -> Integer
---         inFin f' = unFin . f' . toFin
-
 isoFwd :: a <-> b -> a -> b
 isoFwd (Iso g _) = g
 
 isoRev :: a <-> b -> b -> a
 isoRev (Iso _ h) = h
+
+toFin :: HasFin a => a -> Finite (Card a)
+toFin = isoFwd iso
+
+unFin :: HasFin a => Finite (Card a) -> a
+unFin = isoRev iso
 
 {----------------------------------------------------------------------
    A class of types with known finite representations.
@@ -107,41 +96,30 @@ class KnownNat (Card a) => HasFin a where
   iso :: a <-> Finite (Card a)
   iso = Iso toFin unFin
 
-  toFin :: a -> Finite (Card a)
-  toFin = isoFwd iso
-
-  unFin :: Finite (Card a) -> a
-  unFin = isoRev iso
-
-  {-# MINIMAL (iso | (toFin, unFin)) #-}
-
 instance HasFin () where
   type Card () = 1
   iso = Iso (const (Finite 0)) (const ())
 
 instance HasFin Bool where
   type Card Bool = 2
-
-  toFin = Finite . cond 1 0
-
-  unFin (Finite n) = n > 0
+  iso = Iso (Finite . cond 1 0) (\ (Finite n) -> n > 0)
 
 instance KnownNat n => HasFin (Finite n) where
   type Card (Finite n) = n
-
   iso = id
 
 instance (HasFin a, HasFin b) => HasFin (a :+ b) where
   type Card (a :+ b) = Card a + Card b
-
   iso = finSum . (iso +++ iso)
 
 instance (HasFin a, HasFin b) => HasFin (a :* b) where
   type Card (a :* b) = Card a * Card b
-
   iso = finProd . (iso *** iso)
 
--- Domain-typed "arrays"
+{----------------------------------------------------------------------
+  Domain-typed "arrays"
+----------------------------------------------------------------------}
+
 newtype Arr a b = Arr (V.Vector (Card a) b)
 
 instance Functor (Arr a) where
