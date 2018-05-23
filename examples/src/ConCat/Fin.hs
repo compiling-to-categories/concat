f2a38608a81608486c56d87310b5d1e5bf2ef4c5
Conal Elliott <conal@conal.net>
2018-05-23 11:12:33 -0700

    misc tweaks

--- examples/src/ConCat/Fin.hs
+++ examples/src/ConCat/Fin.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE CPP #-}
@@ -13,11 +14,12 @@
 
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
--- {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 -- {-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
+-- {-# OPTIONS_GHC -fconstraint-solver-iterations=10 #-}
 
 #define SMT_NATS
 
+{-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
 #ifdef SMT_NATS
 {-# OPTIONS_GHC -fplugin TypeNatSolver #-}
 #endif
@@ -33,12 +35,13 @@ module ConCat.Fin where
 
 import Data.Proxy (Proxy(..))
 import GHC.TypeLits
+import Data.Type.Bool
 import Control.Arrow ((|||))
 
 -- Experimental
 import Data.Constraint
 
-import ConCat.Misc ((:+))
+import ConCat.Misc ((:*),(:+))
 
 -- Missing from GHC.TypeLits
 infix 4 <, >=, >
@@ -112,11 +115,21 @@ q <+ _ = q
 (<+) = (\\)
 #endif
 
+type u -? v = If (v <=? u) ('Left (u - v)) ('Right (v - u))
+
+type Sub1 u v = If (v <=? u) (u - v) (v - u)
+
+-- type Sub2 u v = Bool :* If (v <=? u) (u - v) (v - u)
+
+
 {--------------------------------------------------------------------
     Finite
 --------------------------------------------------------------------}
 
 data Finite n = forall a. (KnownNat a, a < n) => Finite (Proxy a)
+
+-- TODO: Do we really need 'KnownNat a' here? I think we will, in order to
+-- extract a dynamic natural number for comparisons and arithmetic.
 
 -- -- In GADT notation
 -- data Finite n where Finite :: (KnownNat a, a < n) => Proxy a -> Finite n
@@ -127,22 +140,32 @@ finite = Finite (Proxy @a)
 zero :: (KnownNat n, 0 < n) => Finite n
 zero = finite @0
 
+-- Blows the constraint reduction stack
+-- 
+-- pattern Fi :: forall a n. (KnownNat a, a < n) => Finite n
+-- pattern Fi = Finite (Proxy :: Proxy a)
+
 weakenL :: forall m n. Finite m -> Finite (m + n)
 weakenL (Finite (Proxy :: Proxy a)) = Finite (Proxy @a) <+ ltPlusL @a @m @n
+
+-- Variation
+weaken' :: forall u v. u <= v => Finite u -> Finite v
+weaken' (Finite (Proxy :: Proxy a)) = Finite (Proxy @a)
 
 weakenR :: forall m n. Finite n -> Finite (m + n)
 weakenR (Finite (Proxy :: Proxy b)) = Finite (Proxy @b) <+ ltPlusR @b @m @n
 
--- bumpR :: forall m n. Finite n -> Finite (m + n)
--- bumpR (Finite (Proxy :: Proxy b)) = Finite (Proxy @b) <+ ltPlus2R @b @m @n
+bumpR :: forall m n. KnownNat m => Finite n -> Finite (m + n)
+bumpR (Finite (Proxy :: Proxy b)) = Finite (Proxy @(m + b))
 
 -- #define FProx(a) Finite (Proxy :: Proxy a)
 -- weakenL (FProx(a)) = FProx(a) <+ ltPlusL @a @m @n
 
 type KnownNat2 m n = (KnownNat m, KnownNat n)
 
-sumToFin :: KnownNat2 m n => Finite m :+ Finite n -> Finite (m + n)
-sumToFin = weakenL ||| weakenR
+sumToFin :: KnownNat m => Finite m :+ Finite n -> Finite (m + n)
+-- sumToFin = weakenL ||| weakenR
+sumToFin = weakenL ||| bumpR
 
 finToSum :: KnownNat2 m n => Finite (m + n) -> Finite m :+ Finite n
 finToSum = undefined
