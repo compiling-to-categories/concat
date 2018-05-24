745fcd5c18f72813c1d18ec5e8c60f66381a1dbd
Conal Elliott <conal@conal.net>
2018-05-23 17:19:31 -0700

    rearrange

--- examples/src/ConCat/Fin.hs
+++ examples/src/ConCat/Fin.hs
@@ -40,45 +40,8 @@ type a >= b = b <= a
 type a >  b = b < a
 
 {--------------------------------------------------------------------
-    Finite
+    Comparisons with evidence
 --------------------------------------------------------------------}
-
-data Finite n = forall a. (KnownNat a, a < n) => Finite (Proxy a)
-
--- -- In GADT notation
--- data Finite n where Finite :: (KnownNat a, a < n) => Proxy a -> Finite n
-
--- TODO: Do we really need 'KnownNat a' here? I think we will, in order to
--- extract a dynamic natural number for comparisons and arithmetic.
-
-finite :: forall a n. (KnownNat a, a < n) => Finite n
-finite = Finite (Proxy @a)
-
-zero :: (KnownNat n, 0 < n) => Finite n
-zero = finite @0
-
--- Blows the constraint reduction stack
--- 
--- pattern Fi :: forall a n. (KnownNat a, a < n) => Finite n
--- pattern Fi = Finite (Proxy :: Proxy a)
-
-weakenL :: forall m n. Finite m -> Finite (m + n)
-weakenL (Finite (Proxy :: Proxy a)) = Finite (Proxy @a)
-
--- Variation
-weaken' :: forall u v. u <= v => Finite u -> Finite v
-weaken' (Finite (Proxy :: Proxy a)) = Finite (Proxy @a)
-
-weakenR :: forall m n. Finite n -> Finite (m + n)
-weakenR (Finite (Proxy :: Proxy b)) = Finite (Proxy @b)
-
-bumpR :: forall m n. KnownNat m => Finite n -> Finite (m + n)
-bumpR (Finite (Proxy :: Proxy b)) = Finite (Proxy @(m + b))
-
-type KnownNat2 m n = (KnownNat m, KnownNat n)
-
-sumToFin :: KnownNat m => Finite m :+ Finite n -> Finite (m + n)
-sumToFin = weakenL ||| bumpR
 
 -- | Result of 'compare' with evidence
 data CompareEv u v
@@ -119,6 +82,47 @@ ltEv = case compareEv @u @v of
          CompareLT -> LtT
          CompareEQ -> LtF
          CompareGT -> LtF
+
+{--------------------------------------------------------------------
+    Finite
+--------------------------------------------------------------------}
+
+data Finite n = forall a. (KnownNat a, a < n) => Finite (Proxy a)
+
+-- -- In GADT notation
+-- data Finite n where Finite :: (KnownNat a, a < n) => Proxy a -> Finite n
+
+-- TODO: Do we really need 'KnownNat a' here? I think we will, in order to
+-- extract a dynamic natural number for comparisons and arithmetic.
+
+finite :: forall a n. (KnownNat a, a < n) => Finite n
+finite = Finite (Proxy @a)
+
+zero :: (KnownNat n, 0 < n) => Finite n
+zero = finite @0
+
+-- Blows the constraint reduction stack
+-- 
+-- pattern Fi :: forall a n. (KnownNat a, a < n) => Finite n
+-- pattern Fi = Finite (Proxy :: Proxy a)
+
+weakenL :: forall m n. Finite m -> Finite (m + n)
+weakenL (Finite (Proxy :: Proxy a)) = Finite (Proxy @a)
+
+-- Variation
+weaken' :: forall u v. u <= v => Finite u -> Finite v
+weaken' (Finite (Proxy :: Proxy a)) = Finite (Proxy @a)
+
+weakenR :: forall m n. Finite n -> Finite (m + n)
+weakenR (Finite (Proxy :: Proxy b)) = Finite (Proxy @b)
+
+bumpR :: forall m n. KnownNat m => Finite n -> Finite (m + n)
+bumpR (Finite (Proxy :: Proxy b)) = Finite (Proxy @(m + b))
+
+type KnownNat2 m n = (KnownNat m, KnownNat n)
+
+sumToFin :: KnownNat m => Finite m :+ Finite n -> Finite (m + n)
+sumToFin = weakenL ||| bumpR
 
 finToSum :: forall m n. KnownNat2 m n => Finite (m + n) -> Finite m :+ Finite n
 finToSum (Finite (Proxy :: Proxy a)) =
