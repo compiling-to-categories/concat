0e8ecf6eff165344f5d37266f2e4207f3515fa39
Conal Elliott <conal@conal.net>
2018-05-25 16:25:05 -0700

    Op2 macro

--- examples/src/ConCat/KnownNatOps.hs
+++ examples/src/ConCat/KnownNatOps.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE DataKinds, FlexibleInstances, GADTs, KindSignatures,
              MultiParamTypeClasses, ScopedTypeVariables, TemplateHaskell,
              TypeApplications, TypeFamilies, TypeOperators,
-             UndecidableInstances #-}
+             UndecidableInstances,
+             CPP #-}
 
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -Wno-orphans #-}
@@ -14,6 +15,20 @@ import Data.Proxy            (Proxy (..))
 import GHC.TypeLits
 
 import GHC.TypeLits.KnownNat
+
+#define Op2(Fam,op) \
+type family Fam (m :: Nat) (n :: Nat) :: Nat ; \
+instance (KnownNat a, KnownNat b) => KnownNat2 $(nameToSymbol ''Fam) a b where { \
+  natSing2 = SNatKn (op (natVal (Proxy @a)) (natVal (Proxy @b))) ; \
+  {-# INLINE natSing2 #-} ; \
+  }
+   
+#if 1
+
+Op2(Div,div)
+Op2(Mod,mod)
+
+#else
 
 -- Swiped from GHC.TypNats in base 4.11.0.0
 type family Div (m :: Nat) (n :: Nat) :: Nat
@@ -29,3 +44,4 @@ instance (KnownNat a, KnownNat b) => KnownNat2 $(nameToSymbol ''Mod) a b where
   natSing2 = SNatKn (natVal (Proxy @a) `mod` natVal (Proxy @b))
   {-# INLINE natSing2 #-}
 
+#endif
