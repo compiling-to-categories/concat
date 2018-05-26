e1723a8c50d95cc41d519deadf1418415d032689
Conal Elliott <conal@conal.net>
2018-05-25 17:46:36 -0700

    Op2

--- examples/src/ConCat/KnownNatOps.hs
+++ examples/src/ConCat/KnownNatOps.hs
@@ -11,37 +11,20 @@
 
 module ConCat.KnownNatOps where
 
-import Data.Proxy            (Proxy (..))
 import GHC.TypeLits
-
 import GHC.TypeLits.KnownNat
+import ConCat.Misc (natValAt)
 
 #define Op2(Fam,op) \
-type family Fam (m :: Nat) (n :: Nat) :: Nat ; \
 instance (KnownNat a, KnownNat b) => KnownNat2 $(nameToSymbol ''Fam) a b where { \
-  natSing2 = SNatKn (op (natVal (Proxy @a)) (natVal (Proxy @b))) ; \
+  natSing2 = SNatKn (op (natValAt @a) (natValAt @b)) ; \
   {-# INLINE natSing2 #-} ; \
   }
-   
-#if 1
 
-Op2(Div,div)
-Op2(Mod,mod)
-
-#else
-
--- Swiped from GHC.TypNats in base 4.11.0.0
+-- When the plugin works with GHC 8.4 (base 4.11.0.0), get Div and Mod from
+-- GHC.TypeNat instead of declaring here.
 type family Div (m :: Nat) (n :: Nat) :: Nat
 type family Mod (m :: Nat) (n :: Nat) :: Nat
 
--- See http://hackage.haskell.org/package/ghc-typelits-knownnat/docs/GHC-TypeLits-KnownNat.html
-
-instance (KnownNat a, KnownNat b) => KnownNat2 $(nameToSymbol ''Div) a b where
-  natSing2 = SNatKn (natVal (Proxy @a) `div` natVal (Proxy @b))
-  {-# INLINE natSing2 #-}
-
-instance (KnownNat a, KnownNat b) => KnownNat2 $(nameToSymbol ''Mod) a b where
-  natSing2 = SNatKn (natVal (Proxy @a) `mod` natVal (Proxy @b))
-  {-# INLINE natSing2 #-}
-
-#endif
+Op2(Div,div)
+Op2(Mod,mod)
