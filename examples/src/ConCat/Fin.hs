7ddc76aa3b9aee77bd58be98b36466e8a63d0921
Conal Elliott <conal@conal.net>
2018-05-25 17:46:56 -0700

    Simplify; use KnownNatOps

--- examples/src/ConCat/Fin.hs
+++ examples/src/ConCat/Fin.hs
@@ -17,11 +17,12 @@
 {-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
 
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.KnownNat.Solver #-}
-{-# OPTIONS_GHC -fplugin TypeNatSolver #-}
--- {-# OPTIONS_GHC -fconstraint-solver-iterations=100 #-}
+
+-- -- Allows eliminating some uses of assumeFinite.
+-- {-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
 
 -- | An alternative to Data.Finite from finite-typelits.
--- This version relies on GHC to verify all type arithmetic.
+-- This version relies on GHC to verify type arithmetic where possible.
 module ConCat.Fin where
 
 import Data.Proxy (Proxy(..))
@@ -31,14 +32,21 @@ import Data.Type.Bool
 import Control.Arrow ((|||))
 import Unsafe.Coerce (unsafeCoerce)
 
--- Experimental
 import Data.Constraint
 
 import ConCat.Misc ((:*),(:+),natValAt)
+import ConCat.KnownNatOps (Div, Mod)
 
 {--------------------------------------------------------------------
     Misc
 --------------------------------------------------------------------}
+
+infix 1 |-
+-- | A synonym for '(:-)' but with a looser fixity.
+type (|-) = (:-)
+
+infix 4 >=?
+type a >=? b = b <=? a
 
 infix 4 <?, >?, <, >=, >
 type a <? b = a + 1 <=? b
@@ -56,13 +64,7 @@ unsafeWithEQ r | Refl <- unsafeEqual @a @b = r
 unsafeWithTrue :: forall a r. (a ~ 'True => r) -> r
 unsafeWithTrue r | Refl <- unsafeEqual @a @'True = r
 
--- unsafeWithTrue r = unsafeWithEqual @a @'True @r  -- doesn't type-check
-
-unsafeWithLT :: forall a b r. (a < b => r) -> r
-unsafeWithLT = unsafeWithTrue @(a <? b)
-
-unsafeWithGT :: forall a b r. (a > b => r) -> r
-unsafeWithGT = unsafeWithLT @b @a
+type KnownNat2 m n = (KnownNat m, KnownNat n)
 
 {--------------------------------------------------------------------
     Comparisons with evidence
@@ -76,9 +78,9 @@ data CompareEv u v = (u < v) => CompareLT
 -- 'compare' plus evidence
 compareEv :: forall u v. KnownNat2 u v => CompareEv u v
 compareEv = case natValAt @u `compare` natValAt @v of
-              LT -> unsafeWithLT @u @v CompareLT
-              EQ -> unsafeWithEQ @u @v CompareEQ
-              GT -> unsafeWithGT @u @v CompareGT
+              LT -> unsafeWithTrue @(u <? v) CompareLT
+              EQ -> unsafeWithEQ   @u    @v  CompareEQ
+              GT -> unsafeWithTrue @(u >? v) CompareGT
 
 -- Alternative interface
 compareEv' :: forall u v z. KnownNat2 u v =>
@@ -88,15 +90,6 @@ compareEv' lt eq gt = case compareEv @u @v of
                         CompareEQ -> eq
                         CompareGT -> gt
 
--- (<=) with evidence
-data LeEv u v = (u <= v) => LeT | (u > v) => LeF
-
-leEv :: forall u v. KnownNat2 u v => LeEv u v
-leEv = case compareEv @u @v of
-         CompareLT -> LeT
-         CompareEQ -> LeT
-         CompareGT -> LeF
-
 -- (<) with evidence
 data LtEv u v = (u < v) => LtT | (u >= v) => LtF
 
@@ -104,7 +97,16 @@ ltEv :: forall u v. KnownNat2 u v => LtEv u v
 ltEv = case compareEv @u @v of
          CompareLT -> LtT
          CompareEQ -> LtF
-         CompareGT -> LtF
+         CompareGT -> unsafeWithTrue @(u >=? v) LtF
+
+-- (<=) with evidence
+data LeEv u v = (u <= v) => LeT | (u > v) => LeF
+
+leEv :: forall u v. KnownNat2 u v => LeEv u v
+leEv = case compareEv @u @v of
+         CompareLT -> unsafeWithTrue @(u <=? v) LeT
+         CompareEQ -> LeT
+         CompareGT -> LeF
 
 {--------------------------------------------------------------------
     Finite
@@ -126,20 +128,28 @@ finite = Finite (Proxy @a)
 -- pattern Fi :: forall a n. (KnownNat a, a < n) => Finite n
 -- pattern Fi = Finite (Proxy :: Proxy a)
 
+-- Assume correctly bounded
+assumeFinite' :: forall a m. KnownNat a => Finite m
+assumeFinite' = unsafeWithTrue @(a <? m) (finite @a)
+
+-- | Assume correctly bounded but with an explicit, checked premise. Use only
+-- when @p |- a < m@. The type arguments @p@ and @a@ must be given explicitly,
+-- but usually @m@ can be inferred from context.
+assumeFinite :: forall p a m. (KnownNat a, p) => Finite m
+assumeFinite = assumeFinite' @a @m
+
 weakenL :: forall m n. Finite m -> Finite (m + n)
-weakenL (Finite (Proxy :: Proxy a)) = finite @a
+weakenL (Finite (Proxy :: Proxy a)) = assumeFinite @(a < m) @a
 
 -- Variation
 weaken' :: forall u v. u <= v => Finite u -> Finite v
-weaken' (Finite (Proxy :: Proxy a)) = finite @a
+weaken' (Finite (Proxy :: Proxy a)) = assumeFinite @(a < u) @a
 
 weakenR :: forall m n. Finite n -> Finite (m + n)
-weakenR (Finite (Proxy :: Proxy b)) = finite @b
+weakenR (Finite (Proxy :: Proxy b)) = assumeFinite @(b < n) @b
 
 bumpR :: forall m n. KnownNat m => Finite n -> Finite (m + n)
-bumpR (Finite (Proxy :: Proxy b)) = finite @(m + b)
-
-type KnownNat2 m n = (KnownNat m, KnownNat n)
+bumpR (Finite (Proxy :: Proxy b)) = assumeFinite @(b < n) @(m + b)
 
 sumToFin :: KnownNat m => Finite m :+ Finite n -> Finite (m + n)
 sumToFin = weakenL ||| bumpR
@@ -148,15 +158,11 @@ finToSum :: forall m n. KnownNat2 m n => Finite (m + n) -> Finite m :+ Finite n
 finToSum (Finite (Proxy :: Proxy c)) =
   case ltEv @c @m of
     LtT -> Left  (finite @c)
-    LtF -> Right (finite @(c - m))
+    LtF -> Right (assumeFinite @(c < m + n) @(c - m))
 
--- Alternative definition using leEv
+#if 0
 
-finToSum' :: forall m n. KnownNat2 m n => Finite (m + n) -> Finite m :+ Finite n
-finToSum' (Finite (Proxy :: Proxy c)) =
-  case leEv @m @c of
-    LeF -> Left  (finite @c)
-    LeT -> Right (finite @(c - m))
+-- Look for duality between sumToFin and finToSum.
 
 -- With some inlining
 sumToFin' :: forall m n. KnownNat m => Finite m :+ Finite n -> Finite (m + n)
@@ -170,14 +176,13 @@ finToSum'' (Finite (Proxy :: Proxy c)) =
     LtT -> Left  (Finite (Proxy :: Proxy    c   ))
     LtF -> Right (Finite (Proxy :: Proxy (c - m)))
 
--- finToProd :: forall m n. KnownNat n => Finite m :* Finite n -> Finite (m * n)
--- finToProd (Finite (Proxy :: Proxy a), Finite (Proxy :: Proxy b)) =
---   Finite (Proxy :: Proxy (a * n + b)) \\ prodBound @a @b @m @n
+#endif
 
--- "SMT solver: ... logic does not support nonlinear arithmetic."
+finToProd :: forall m n. KnownNat n => Finite m :* Finite n -> Finite (m * n)
+finToProd (Finite (Proxy :: Proxy a), Finite (Proxy :: Proxy b)) =
+  assumeFinite @(a < m, b < n) @(a * n + b)
 
-sumBoundR :: (a < m, b < n) :- (m + b < m + n)
-sumBoundR = Sub Dict  -- Thanks to TypeNatSolver, I presume.
-
--- prodBound :: forall a b m n. (a < m, b < n) :- (a * n + b < m * n)
--- prodBound = Sub (unsafeWithLT @(a * n + b) @(m * n) Dict)
+prodToFin :: forall m n. KnownNat n => Finite (m * n) -> Finite m :* Finite n
+prodToFin (Finite (Proxy :: Proxy c)) =
+  ( assumeFinite @(c < m * n) @(c `Div` n) @m
+  , assumeFinite @(c < m * n) @(c `Mod` n) @n )
