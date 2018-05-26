916e268a833524ba7f556528049a85beb24c204c
Conal Elliott <conal@conal.net>
2018-05-26 15:34:58 -0700

    fill in

--- examples/src/ConCat/Isomorphism.hs
+++ examples/src/ConCat/Isomorphism.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 
 {-# OPTIONS_GHC -Wall #-}
@@ -12,7 +13,25 @@ import qualified ConCat.Category
 
 data Iso k a b = Iso (a `k` b) (b `k` a)
 
+inverse :: Iso k a b -> Iso k b a
+inverse (Iso f f') = Iso f' f
+
 instance Category k => Category (Iso k) where
+  type Ok (Iso k) = Ok k
   id = Iso id id
   Iso g g' . Iso f f' = Iso (g . f) (f' . g')
 
+instance MonoidalPCat k => MonoidalPCat (Iso k) where
+  Iso f f' *** Iso g g' = Iso (f *** g) (f' *** g')
+
+instance MonoidalSCat k => MonoidalSCat (Iso k) where
+  Iso f f' +++ Iso g g' = Iso (f +++ g) (f' +++ g')
+
+isoFwd :: Iso k a b -> (a `k` b)
+isoFwd (Iso g _) = g
+
+isoRev :: Iso k a b -> (b `k` a)
+isoRev (Iso _ h) = h
+
+infix 0 <->
+type (<->) = Iso (->)
