d9892ab9352d299a80bf07eaaa101e3d081d5302
Conal Elliott <conal@conal.net>
2018-06-05 15:04:49 -0700

    INLINE

--- examples/src/ConCat/Isomorphism.hs
+++ examples/src/ConCat/Isomorphism.hs
@@ -9,25 +9,39 @@
 module ConCat.Isomorphism where
 
 import Prelude hiding (id, (.), const)  -- Coming from ConCat.AltCat.
+
+import ConCat.Misc ((:*))
 import ConCat.AltCat
 import qualified ConCat.Category
+import ConCat.Rep
 
 infix 0 :<->
 data Iso k a b = (a `k` b) :<-> (b `k` a)
 
+-- Helpful?
+instance HasRep (Iso k a b) where
+  type Rep (Iso k a b) = (a `k` b) :* (b `k` a)
+  abst (f,f') = f :<-> f'
+  repr (f :<-> f') = (f,f')
+
 inverse :: Iso k a b -> Iso k b a
 inverse (f :<-> f') = f' :<-> f
+{-# INLINE inverse #-}
 
 instance Category k => Category (Iso k) where
   type Ok (Iso k) = Ok k
   id = id :<-> id
   (g :<-> g') . (f :<-> f') = (g . f) :<-> (f' . g')
+  {- INLINE id #-}
+  {- INLINE (.) #-}
 
 instance MonoidalPCat k => MonoidalPCat (Iso k) where
   (f :<-> f') *** (g :<-> g') = (f *** g) :<-> (f' *** g')
+  {- INLINE (***) #-}
 
 instance MonoidalSCat k => MonoidalSCat (Iso k) where
   (f :<-> f') +++ (g :<-> g') = (f +++ g) :<-> (f' +++ g')
+  {- INLINE (+++) #-}
 
 isoFwd :: Iso k a b -> (a `k` b)
 isoFwd (f :<-> _) = f
