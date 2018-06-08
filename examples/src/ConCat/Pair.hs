e5e43f435db12896d36fa283f75a944c79ed9cf3
Conal Elliott <conal@conal.net>
2018-06-08 15:03:22 -0700

    SpecialPair

--- examples/src/ConCat/Pair.hs
+++ examples/src/ConCat/Pair.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DeriveTraversable #-}
@@ -13,10 +14,10 @@
 
 {-# OPTIONS_GHC -Wall #-}
 
-{-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
+-- {-# OPTIONS_GHC -fno-warn-unused-imports #-} -- TEMP
 -- {-# OPTIONS_GHC -fno-warn-unused-binds   #-} -- TEMP
 
--- #define SpecialPair
+#define SpecialPair
 
 ----------------------------------------------------------------------
 -- | Uniform pairs
@@ -29,20 +30,23 @@ module ConCat.Pair
 #else
     Pair
 #endif
-  , pattern (:#)
+  -- , pattern (:#)
   ) where
 
-import GHC.Generics (Par1(..),(:*:)(..))
+-- import GHC.Generics (Par1(..),(:*:)(..))
 #ifdef SpecialPair
-import GHC.Generics (Generic1(..))
+-- import GHC.Generics (Generic1(..))
 #endif
 
 #ifdef SpecialPair
+import Prelude hiding (zipWith)
+
+import Data.Monoid ((<>))
 import Control.Applicative (liftA2)
 import Data.Key
 import Data.Pointed
 import Data.Distributive (Distributive(..))
-import Data.Functor.Rep (Representable(tabulate,index))
+import Data.Functor.Rep (Representable(tabulate,index),distributeRep)
 import qualified Data.Functor.Rep as R
 import Control.Newtype (Newtype(..))
 import Data.Constraint (Dict(..),(:-)(..))
@@ -54,7 +58,7 @@ import ConCat.Sized
 import ConCat.Scan
 import ConCat.Circuit ((:>),GenBuses(..),Buses(..),BusesM,Ty(..),abstB)
 import ConCat.Free.VectorSpace (HasV(..))
-import ConCat.AltCat (type (|-)(..),Ok)
+import ConCat.AltCat (type (|-)(..),Additive1(..),OkFunctor(..))
 #endif
 
 #ifndef SpecialPair
@@ -67,26 +71,33 @@ pattern x :# y = Par1 x :*: Par1 y
 
 #else
 
-type GPair = Par1 :*: Par1
+-- type GPair = Par1 :*: Par1
 
-newtype Pair a = Pair (GPair a)
+-- newtype Pair a = Pair (GPair a)
 
-instance Newtype (Pair a) where
-  type O (Pair a) = GPair a
-  pack as = Pair as
-  unpack (Pair as) = as
+-- instance Newtype (Pair a) where
+--   type O (Pair a) = GPair a
+--   pack as = Pair as
+--   unpack (Pair as) = as
 
-instance Generic1 Pair where
-  type Rep1 Pair = GPair
-  to1 p = Pair p
-  from1 (Pair p) = p
-
-instance Additive1 Pair where additive1 = Entail (Sub Dict)
+-- instance Generic1 Pair where
+--   type Rep1 Pair = GPair
+--   to1 p = Pair p
+--   from1 (Pair p) = p
 
 infixl 1 :#
 
-pattern (:#) :: a -> a -> Pair a
-pattern x :# y = Pair (Par1 x :*: Par1 y)
+data Pair a = a :# a
+
+instance Newtype (Pair a) where
+  type O (Pair a) = a :* a
+  pack (a,a') = a :# a'
+  unpack (a :# a') = (a,a')
+
+instance Additive1 Pair where additive1 = Entail (Sub Dict)
+
+-- pattern (:#) :: a -> a -> Pair a
+-- pattern x :# y = Pair (Par1 x :*: Par1 y)
 
 instance Show a => Show (Pair a) where
   showsPrec p = \ (x :# y) -> showParen (p >= 1) $ showsPrec 1 x . showString " :# " . showsPrec 1 y
@@ -105,20 +116,41 @@ instance HasRep (Pair a) where
 -- TODO: try again with a later GHC.
 
 deriving instance Functor Pair
-deriving instance Applicative Pair
-deriving instance Monad Pair
 deriving instance Foldable Pair
 deriving instance Traversable Pair
 
-deriving instance Pointed Pair
+-- deriving instance Applicative Pair
+-- deriving instance Monad Pair
+-- deriving instance Pointed Pair
+
+instance Pointed Pair where point a = a :# a
+
+instance Zip Pair where
+  zipWith f (a :# a') (b :# b') = f a b :# f a' b'
+
+instance Applicative Pair where
+  pure = point
+  (<*>) = zipWith ($)
+
+-- instance Monad Pair where ...
+
+instance Additive a => Additive (Pair a)
+
+-- instance Distributive Pair where
+--   distribute ps = pack (distribute (unpack <$> ps))
+
+-- instance Representable Pair where
+--   type Rep Pair = R.Rep GPair
+--   tabulate f = Pair (tabulate f)
+--   index (Pair xs) = R.index xs
 
 instance Distributive Pair where
-  distribute ps = pack (distribute (unpack <$> ps))
+  distribute = distributeRep
 
 instance Representable Pair where
-  type Rep Pair = R.Rep GPair
-  tabulate f = Pair (tabulate f)
-  index (Pair xs) = R.index xs
+  type Rep Pair = Bool
+  tabulate f = f False :# f True
+  index (f :# t) c = if c then t else f
 
 instance HasV s a => HasV s (Pair a)
 
@@ -148,8 +180,11 @@ instance OkFunctor (:>) Pair where
 -- Without these NOINLINE pragmas, GHC's typechecker does exponential work for
 -- binary trees.
 
-instance LScan Pair
+-- instance LScan Pair
+-- deriving instance Zip Pair
 
-deriving instance Zip Pair
+instance LScan Pair where
+  lscan :: Monoid a => Pair a -> Pair a :* a
+  lscan (a :# b) = (mempty :# a, a <> b)
 
 #endif
