91f02c35777bb7c7b93a245db26d9e14ea59ef55
Conal Elliott <conal@conal.net>
2018-09-03 13:50:35 -0700

    experiments

--- known/src/ConCat/Known.hs
+++ known/src/ConCat/Known.hs
@@ -2,6 +2,16 @@
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE TypeOperators #-}
 
+-- #define KNOWABLE
+
+#ifdef KNOWABLE
+{-# LANGUAGE MonoLocalBinds #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+#endif
+
 {-# OPTIONS_GHC -Wall #-}
 -- {-# OPTIONS_GHC -Wno-unused-imports #-} -- TEMP
 
@@ -25,3 +35,27 @@ nm = Sub Dict
 
 KNOW(knownAdd,+)
 KNOW(knownMul,GHC.TypeLits.*)
+
+{--------------------------------------------------------------------
+    Experiment
+--------------------------------------------------------------------}
+
+#ifdef KNOWABLE
+
+class KnowableNat n where knownNat :: Dict (KnownNat n)
+
+instance {-# OVERLAPPABLE #-} KnownNat n => KnowableNat n where knownNat = Dict
+
+-- type KnownNat2 m n = (KnownNat m, KnownNat n)
+
+-- instance KnownNat2 m n => KnowableNat (m + n) where knownNat = Dict \\ knownAdd @m @n
+-- instance KnownNat2 m n => KnowableNat (m * n) where knownNat = Dict \\ knownMul @m @n
+
+type KnowableNat2 m n = (KnowableNat m, KnowableNat n)
+
+instance KnowableNat2 m n => KnowableNat (m + n) where knownNat = Dict \\ knownAdd @m @n
+instance KnowableNat2 m n => KnowableNat (m * n) where knownNat = Dict \\ knownMul @m @n
+
+-- Illegal type synonym family application in instance
+
+#endif
