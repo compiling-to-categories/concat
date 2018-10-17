82ebefccb34f7b787749d2e216fb44f1471dcf50
Conal Elliott <conal@conal.net>
2018-10-16 21:03:39 -0700

    keep just one version of (.). see git history or today's notes for others.

--- examples/src/ConCat/Distrib.hs
+++ examples/src/ConCat/Distrib.hs
@@ -26,29 +26,4 @@ instance Category Distrib where
   id = Distrib (\ a -> M.singleton a 1)
   Distrib g . Distrib f = Distrib h
    where
-     -- h = M.fromListWith (+) . concatMap (rescale . first (M.toList . g)) . M.toList . f
-     -- rescale (w,q) = second (q *) <$> w
-
-     -- h a = M.fromListWith (+) [ (c,p*q) | (b,p) <- M.toList (f a), (c,q) <- M.toList (g b) ]
-
      h a = M.unionsWith (+) [ (p *) <$> g b | (b,p) <- M.toList (f a) ]
-
-     -- h a = M.unionsWith (+) (w <$> M.toList (f a)) where w (b,p) = (p *) <$> g b
-
-     -- h = M.unionsWith (+) . fmap w . M.toList . f where w (b,p) = (p *) <$> g b
-
-     -- h = M.unionsWith (+) . fmap (\ (b,p) -> (p *) <$> g b) . M.toList . f
-
-#if 0
-
-f :: a -> Map b R
-g :: b -> Map c R
-
-toList . f :: a -> [b :* R]
-map (first g) . toList . f :: a -> [Map c R :* R]
-map (first (toList . g)) . toList . f :: a -> [[c :* R] :* R]
-map (rescale . first (toList . g)) . toList . f :: a -> [[c :* R]]
-concatMap (rescale . first (toList . g)) . toList . f :: a -> [c :* R]
-fromListWith . concatMap (rescale . first (toList . g)) . toList . f :: a -> [c :* R]
-
-#endif
