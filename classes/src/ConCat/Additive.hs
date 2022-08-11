48453ec7ac2129e5b44aba449e6ae063261fbde5
Mike Sperber <sperber@deinprogramm.de>
2022-08-11 14:58:52 +0200

    Add missing imports for Additive instances
    ... from previous commit.

--- classes/src/ConCat/Additive.hs
+++ classes/src/ConCat/Additive.hs
@@ -26,6 +26,8 @@ import Data.Monoid (Monoid(..), Sum(..), Product(..))
 import Data.Semigroup (Semigroup(..))
 import Data.Complex hiding (magnitude)
 import Data.Ratio
+import Data.Int
+import Data.Word
 import Foreign.C.Types (CSChar, CInt, CShort, CLong, CLLong, CIntMax, CFloat, CDouble)
 import GHC.Generics (U1(..),Par1(..),(:*:)(..),(:.:)(..))
 import GHC.TypeLits (KnownNat)
